322b651e38c285f94e6bb33e008614bcf6d912b7
Rishabh Gupta <rishabhgupta2804@gmail.com>
2022-10-28 00:50:13 +0530

    Added solution - LeetHub

---
+++ Sequence Fun - GFG/sequence-fun.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution ob = new Solution();
+            int ans = ob.NthTerm(n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    int mod = 1000000007;
+    
+    public int NthTerm(int n)
+    {
+        if(n == 1)
+            return 2;
+    
+        long ans = NthTerm(n - 1);
+        ans = ((ans % mod) * (n % mod)) % mod;
+        ans ++;
+        ans %= mod;
+        
+        return (int)(ans % mod);
+    }
+}
