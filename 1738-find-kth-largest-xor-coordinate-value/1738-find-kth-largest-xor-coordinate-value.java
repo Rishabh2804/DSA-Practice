8feeeb67b49a62c838e2615c957e799b42d4d7fc
Rishabh Gupta <rishabhgupta2804@gmail.com>
2022-10-31 13:56:18 +0530

    Time: 300 ms (75.00%), Space: 170.4 MB (89.19%) - LeetHub

--- 1738-find-kth-largest-xor-coordinate-value/1738-find-kth-largest-xor-coordinate-value.java
+++ 1738-find-kth-largest-xor-coordinate-value/1738-find-kth-largest-xor-coordinate-value.java
@@ -4,27 +4,43 @@ class Solution {
         int m = matrix.length;
         int n = matrix[0].length;
         
-        int[][] rowPrefixXor = new int[m][n];
-        for(int i = 0; i < m; ++i){
-            rowPrefixXor[i][0] = matrix[i][0];
-            for(int j = 1; j < n; ++j)
-                rowPrefixXor[i][j] = rowPrefixXor[i][j - 1] ^ matrix[i][j];            
-        }
+        // int[][] rowPrefixXor = new int[m][n];
+        // for(int i = 0; i < m; ++i){
+        //     rowPrefixXor[i][0] = matrix[i][0];
+        //     for(int j = 1; j < n; ++j)
+        //         rowPrefixXor[i][j] = rowPrefixXor[i][j - 1] ^ matrix[i][j];            
+        // }
 
         PriorityQueue<Integer> xors = new PriorityQueue<>();
 
         int[][] gridPrefixXor = new int[m][n];
-        for(int j = 0; j < n; ++j) {
-            gridPrefixXor[0][j] = rowPrefixXor[0][j];
-            xors.add(gridPrefixXor[0][j]);
+        gridPrefixXor[0][0] = matrix[0][0];
+        xors.add(gridPrefixXor[0][0]);
+        
+        for(int i = 1; i < m; ++i){
+            gridPrefixXor[i][0] = gridPrefixXor[i - 1][0] ^ matrix[i][0];
+            
+            xors.add(gridPrefixXor[i][0]);
                 
-                if(xors.size() > k)
-                    xors.poll();
+            if(xors.size() > k)
+                xors.poll();
         }
         
+        for(int j = 1; j < n; ++j){
+            gridPrefixXor[0][j] = gridPrefixXor[0][j - 1] ^ matrix[0][j];
+            
+            xors.add(gridPrefixXor[0][j]);
+                
+            if(xors.size() > k)
+                xors.poll();
+        }    
+        
         for(int i = 1; i < m; ++i) {
-            for(int j = 0; j < n; ++j) {
-                gridPrefixXor[i][j] = gridPrefixXor[i - 1][j] ^ rowPrefixXor[i][j];
+            for(int j = 1; j < n; ++j) {
+                gridPrefixXor[i][j] = gridPrefixXor[i - 1][j] 
+                                      ^ gridPrefixXor[i][j - 1]
+                                      ^ gridPrefixXor[i - 1][j - 1]
+                                      ^ matrix[i][j];
                 
                 xors.add(gridPrefixXor[i][j]);
                 
