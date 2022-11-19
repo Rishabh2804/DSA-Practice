b454255419ade9869d8cfe1a55aa439b4d043fd1
Rishabh Gupta <rishabhgupta2804@gmail.com>
2022-11-20 01:33:26 +0530

    Time: 39 ms (58.06%), Space: 54.7 MB (33.87%) - LeetHub

---
+++ 0587-erect-the-fence/0587-erect-the-fence.java
@@ -0,0 +1,35 @@
+public class Solution {
+    public int orientation(int[] p, int[] q, int[] r) {
+        return (q[1] - p[1]) * (r[0] - q[0]) 
+             - (q[0] - p[0]) * (r[1] - q[1]);
+    }
+
+    // public boolean inBetween(int[] p, int[] i, int[] q) {
+    //     boolean a = i[0] >= p[0] && i[0] <= q[0] || i[0] <= p[0] && i[0] >= q[0];
+    //     boolean b = i[1] >= p[1] && i[1] <= q[1] || i[1] <= p[1] && i[1] >= q[1];
+    //     return a && b;
+    // }
+
+    public int[][] outerTrees(int[][] points) {
+        Arrays.sort(points, new Comparator<int[]> () {
+            public int compare(int[] p, int[] q) {
+                return q[0] - p[0] == 0 ? q[1] - p[1] : q[0] - p[0];
+            }
+        });
+        Stack<int[]> hull = new Stack<>();
+        for (int i = 0; i < points.length; i++) {
+            while (hull.size() >= 2 && orientation(hull.get(hull.size() - 2), hull.get(hull.size() - 1), points[i]) > 0)
+                hull.pop();
+            hull.push(points[i]);
+        }
+        hull.pop();
+        for (int i = points.length - 1; i >= 0; i--) {
+            while (hull.size() >= 2 && orientation(hull.get(hull.size() - 2), hull.get(hull.size() - 1), points[i]) > 0)
+                hull.pop();
+            hull.push(points[i]);
+        }
+        // remove redundant elements from the stack
+        HashSet<int[]> ret = new HashSet<>(hull);
+        return ret.toArray(new int[ret.size()][]);
+    }
+}
