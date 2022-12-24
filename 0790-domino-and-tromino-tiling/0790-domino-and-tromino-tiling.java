f44dc303d3bdfd485b0a4d54a092fcabed92775f
Rishabh Gupta <rishabhgupta2804@gmail.com>
2022-12-24 16:36:27 +0530

    Time: 43 ms (5.07%), Space: 39.5 MB (78.02%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.java
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.java
@@ -1,9 +1,10 @@
 class Solution {
-    static int mod = 1000000007;
     
     public int numTilings(int n) {
         if(n == 1)
             return 1;
+        
+        int mod = 1000000007;
         
         int[] dp = new int[n + 1];
         dp[0] = 1;
