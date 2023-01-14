83bbd5c9324d05c99509bfb046af390d428cfb18
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-01-14 14:35:35 +0530

    Time: 2 ms (88.07%), Space: 40.7 MB (90.91%) - LeetHub

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.java
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.java
@@ -1,8 +1,8 @@
 class Solution {
+    static int[] parent = new int[26];
+    static int[] dict = new int[26];
+    
     public String smallestEquivalentString(String s1, String s2, String baseStr) {
-
-        int[] parent = new int[26];
-        int[] dict = new int[26];
 
         for (int i = 0; i < 26; ++i) {
             parent[i] = i;
@@ -13,13 +13,13 @@ class Solution {
             int idx1 = s1.charAt(i) - 'a';
             int idx2 = s2.charAt(i) - 'a';
 
-            union(idx1, idx2, dict, parent);    
+            union(idx1, idx2);    
         }
 
         StringBuilder eqStr = new StringBuilder();
         for (int i = 0; i < baseStr.length(); ++i) {
             int idx = baseStr.charAt(i) - 'a';
-            int par = find(idx, parent);
+            int par = find(idx);
  
             eqStr.append((char)(dict[par] + 'a'));
         }
@@ -27,21 +27,21 @@ class Solution {
         return eqStr.toString();
     }
 
-    private void union(int x, int y, int[] dict, int[] parent) {
-        int par1 = find(x, parent);
-        int par2 = find(y, parent);
+    private void union(int x, int y) {
+        int par1 = find(x);
+        int par2 = find(y);
         
         int lexoFirst = Math.min(x, y);        
         dict[par1] = Math.min(dict[par1], lexoFirst);    
         dict[par1] = Math.min(dict[par1], dict[par2]);
         
-        parent[find(y, parent)] = find(x, parent);        
+        parent[find(y)] = find(x);        
     }
 
-    private int find(int x, int[] parent) {
+    private int find(int x) {
         if(x == parent[x])
             return x;
         
-        return find(parent[x], parent);
+        return find(parent[x]);
     }
 }
