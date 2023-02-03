4324ec27098668395aed8fb363fa797bd7b494d6
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-02-04 02:14:40 +0530

    Time: 9 ms (93.71%), Space: 41.3 MB (81.12%) - LeetHub

---
+++ 1114-print-in-order/1114-print-in-order.java
@@ -0,0 +1,44 @@
+class Foo {
+
+        int a;
+
+        public Foo() {
+            a = 1;
+        }
+
+        public void first(Runnable printFirst) throws InterruptedException {
+
+            synchronized (this) {
+                printFirst.run();
+                a = 2;
+                notifyAll();
+            }
+            // printFirst.run() outputs "first". Do not change or remove this line.
+
+        }
+
+        public void second(Runnable printSecond) throws InterruptedException {
+
+            synchronized (this) {
+                while (a < 2)
+                    wait();
+                // printSecond.run() outputs "second". Do not change or remove this line.
+                printSecond.run();
+                a = 3;
+                notifyAll();
+            }
+        }
+
+        public void third(Runnable printThird) throws InterruptedException {
+
+            synchronized (this) {
+                while (a < 3)
+                    wait();
+                // printThird.run() outputs "third". Do not change or remove this line.
+                printThird.run();   
+                a = 4;
+                notifyAll();             
+            }
+
+        }
+    }
