48c88e49aa38f192dcac6d7042489db39f06a779
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-02-04 13:47:24 +0530

    Time: 10 ms (72.34%), Space: 41.9 MB (35.37%) - LeetHub

--- 1114-print-in-order/1114-print-in-order.java
+++ 1114-print-in-order/1114-print-in-order.java
@@ -1,42 +1,27 @@
 class Foo {
 
-        int a;
+        volatile int a;
 
         public Foo() {
             a = 1;
         }
 
         public void first(Runnable printFirst) throws InterruptedException {
-
-            synchronized (this) {
-                printFirst.run();
-                a = 2;
-                notifyAll();
-            }
-            // printFirst.run() outputs "first". Do not change or remove this line.
-
+            while(a != 1);
+            printFirst.run();
+            a = 2;            
         }
 
         public void second(Runnable printSecond) throws InterruptedException {
 
-            synchronized (this) {
-                while (a < 2)
-                    wait();
-                // printSecond.run() outputs "second". Do not change or remove this line.
-                printSecond.run();
-                a = 3;
-                notifyAll();
-            }
+            while (a != 2);                    
+            printSecond.run();
+            a = 3;            
         }
 
         public void third(Runnable printThird) throws InterruptedException {
 
-            synchronized (this) {
-                while (a < 3)
-                    wait();
-                // printThird.run() outputs "third". Do not change or remove this line.
-                printThird.run();                                              
-            }
-
+            while (a != 3);
+            printThird.run();                
         }
     }
