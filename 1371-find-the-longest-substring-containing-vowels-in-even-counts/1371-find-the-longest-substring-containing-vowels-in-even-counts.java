a756464873d7cc7fccec5cf8795365f492e184a8
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-02-08 03:03:14 +0530

    Time: 3665 ms (5.49%), Space: 50.4 MB (31.87%) - LeetHub

---
+++ 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int findTheLongestSubstring(String s) {
+        
+        int[] masks = new int[32];
+        Arrays.fill(masks, -1);
+        masks[0] = 0;
+        
+        int[] strMasks = new int[s.length()];
+        strMasks[0] = vowelMask(s.charAt(0));
+        masks[strMasks[0]] = 0;
+        
+        int maxLength = 0;
+        if(strMasks[0] == 0)
+            maxLength = 1;
+        
+        for(int i = 1; i < s.length(); ++i){
+            strMasks[i] = strMasks[i - 1] ^ vowelMask(s.charAt(i));
+            
+            if(strMasks[i] == 0)
+                maxLength = i + 1;            
+            else if(masks[strMasks[i]] != -1) {
+                int length = i - masks[strMasks[i]];
+                maxLength = Math.max(maxLength, length);
+            } else masks[strMasks[i]] = i;
+            
+            System.out.println(i + " " + strMasks[i]  + " " + masks[strMasks[i]]);
+        }                
+        
+        return maxLength;
+    }
+    
+    public int vowelMask(char c){
+        if(c == 'a')      return 1;
+        else if(c == 'e') return 2;
+        else if(c == 'i') return 4;
+        else if(c == 'o') return 8;
+        else if(c == 'u') return 16;
+        else            return 0;
+    }
+}
