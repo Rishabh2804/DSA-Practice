24776d42cc2bcc622947825a9650504ba8858440
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-02-08 10:55:47 +0530

    Time: 91 ms (18.40%), Space: 42.7 MB (75.83%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int jump(int[] nums) {
+        
+        int[] jumpCount = new int[nums.length];
+        Arrays.fill(jumpCount, Integer.MAX_VALUE - 1);
+        
+        jumpCount[0] = 0;
+        for(int i = 0; i < nums.length; ++i){                        
+            for(int j = i + 1; j < Math.min(i + nums[i] + 1, nums.length); ++j)
+                jumpCount[j] = Math.min(jumpCount[j], 1 + jumpCount[i]);            
+        }
+        
+        return jumpCount[nums.length - 1];
+    }
+}
