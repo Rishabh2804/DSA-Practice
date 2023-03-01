02101fec98d53b8f00898a135bf9fff561b67c8b
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-03-01 14:16:16 +0530

    Time: 28 ms (79.38%), Space: 51.4 MB (93.66%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,46 +1,41 @@
 class Solution {
-    public int[] sortArray(int[] nums) {
-        // quickSort(0, nums.length - 1, nums);
-        Arrays.sort(nums);
+    
+    static int[] buffer;
+    
+    public int[] sortArray(int[] nums) {     
+        buffer = new int[nums.length];
+        mergeSort(0, nums.length - 1, nums);
         return nums;
     }
     
-    public void quickSort(int si, int ei, int[] arr) {
+    public void mergeSort(int si, int ei, int[] arr){
         if(si >= ei)
             return;
         
-        int pivot = partition(si, ei, arr);
-        quickSort(si, pivot - 1, arr);
-        quickSort(pivot + 1, ei, arr);
+        int mid = (si + ei) / 2;
+        mergeSort(si, mid, arr);
+        mergeSort(mid + 1, ei, arr);
+        
+        int i = si;
+        int j = mid + 1;
+        int k = si;
+        
+        while(i <= mid && j <= ei){
+            if(arr[i] <= arr[j])
+                buffer[k++] = arr[i++];
+            else
+                buffer[k++] = arr[j++];
+        }
+        
+        while(i <= mid)
+            buffer[k++] = arr[i++];
+        
+        while(j <= ei)
+            buffer[k++] = arr[j++];
+        
+        for(int ii = si; ii <= ei; ++ii)
+            arr[ii] = buffer[ii];
     }
     
-    public int partition(int si, int ei, int[] arr){
-        int mid = (si + ei) / 2;
-        
-        int count = si;
-        for(int i = si; i <= ei; ++i){
-            if(arr[i] < arr[mid])
-                count ++;
-        }
-        
-        int temp = arr[mid];
-        arr[mid] = arr[count];
-        arr[count] = temp;
-        
-        int i = si, j = ei;
-        while(i < count){
-            if(arr[i] >= arr[count]){
-                while(j > count && arr[j] >= arr[count])
-                    j--;
-
-                int tmp = arr[j];
-                arr[j] = arr[i];
-                arr[i] = tmp;
-            }
-
-            i++;                
-        }
-        
-        return count;    
-    }
+    
 }
