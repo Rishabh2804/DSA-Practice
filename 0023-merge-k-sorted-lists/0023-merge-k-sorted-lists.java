206beca7d1c4dd034ded96b815564fc3a05ba707
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-03-12 12:32:19 +0530

    Time: 5 ms (71.47%), Space: 44.7 MB (20.67%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -1,32 +1,20 @@
 class Solution {
 
-    static class ListSorter<E> extends PriorityQueue<E> {
-
-        private static final int DEFAULT_INITIAL_CAPACITY = 11;
-
-        ListSorter(Comparator<E> c) {
-            super(DEFAULT_INITIAL_CAPACITY, c);
-        }
-
-        @Override
-        public boolean add(E e) {
-            if (e == null)
-                return false;
-
-            return offer(e);
-        }
-
-    }
-
     public ListNode mergeKLists(ListNode[] lists) {
         if (lists.length == 0)
             return null;
 
-        ListSorter<ListNode> nodes = new ListSorter<>((a, b) -> a.val - b.val);
-        Collections.addAll(nodes, lists);
-
+        PriorityQueue<ListNode> nodes = new PriorityQueue<>((a, b) -> a.val - b.val);
+        for (ListNode head : lists) {
+            if (head != null)
+                nodes.add(head);
+        }
+        
         ListNode head = nodes.poll();
-        if(head != null)
+        if (head == null)
+            return head;
+            
+        if(head.next != null)
             nodes.add(head.next);
 
         ListNode tail = head;
@@ -35,7 +23,8 @@ class Solution {
             tail.next = curr;
             tail = tail.next;
 
-            nodes.add(curr.next);
+            if(curr.next != null)
+                nodes.add(curr.next);
         }
 
         return head;
