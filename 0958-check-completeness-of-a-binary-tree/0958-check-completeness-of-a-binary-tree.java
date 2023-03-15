3db07a107e8c4856765a8336a3e86ebd3af51951
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-03-15 18:49:49 +0530

    Time: 1 ms (83.31%), Space: 42.1 MB (27.50%) - LeetHub

--- 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.java
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.java
@@ -19,7 +19,7 @@ class Solution {
             return true;
         
         Queue<TreeNode> nodes = new LinkedList<>();
-        nodes.add(root);
+        nodes.offer(root);
         
         boolean nullFound = false;
         while(!nodes.isEmpty()){
@@ -32,13 +32,13 @@ class Solution {
                     nullFound = true;
                 else if(nullFound)
                     return false;
-                else nodes.add(curr.left);
+                else nodes.offer(curr.left);
                 
                 if(curr.right == null)
                     nullFound = true;
                 else if(nullFound)
                     return false;
-                else nodes.add(curr.right);
+                else nodes.offer(curr.right);
             }
         }
         
