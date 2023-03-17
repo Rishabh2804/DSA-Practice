f5d6a3c1e34230bc128511f3a6009d16b90523c7
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-03-17 14:24:14 +0530

    Time: 35 ms (94.22%), Space: 51.9 MB (61.05%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -1,74 +1,66 @@
 class Trie {
+    
+    boolean isEnd;
+    Trie[] children;
 
-    static class TrieNode{
-        boolean isEnd;
-        TrieNode[] children;
-        
-        public TrieNode(){
-            isEnd = false;
-            children = new TrieNode[26];
-        }
-    }
-    
-    TrieNode root;
-    
     public Trie() {
-        root = new TrieNode();
-        root.isEnd = true;
+        children = new Trie[26];
+        isEnd = false;
     }
     
     public void insert(String word) {
-        insert(root, 0, word);
+        insert(word, 0);
     }
-    
-    private void insert(TrieNode root, int i, String word){
-        if(i >= word.length()){
-            root.isEnd = true;
+
+    private void insert(String word, int i) {
+        if (i == word.length()) {
+            isEnd = true;
             return;
         }
-        
-        int childIdx = word.charAt(i) - 'a';
-        if(root.children[childIdx] == null)
-            root.children[childIdx] = new TrieNode();                
-        
-        insert(root.children[childIdx], i + 1, word);
+
+        char c = word.charAt(i);
+
+        if (children[c - 'a'] == null) {
+            children[c - 'a'] = new Trie();
+        }
+
+        children[c - 'a'].insert(word, i + 1);
     }
     
     public boolean search(String word) {
-        return search(root, 0, word);   
+        return search(word, 0);
     }
-    
-    private boolean search(TrieNode root, int i, String word){            
-        if(i >= word.length())
-            return root.isEnd;
-        
-        int childIdx = word.charAt(i) - 'a';
-        if(root.children[childIdx] == null)
+
+    private boolean search(String word, int i) {
+        if (word.length() == i) {
+            return isEnd;
+        }
+
+        char c = word.charAt(i);
+
+        if (children[c - 'a'] == null) {
             return false;
-        
-        return search(root.children[childIdx], i + 1, word);        
+        }
+
+        return children[c - 'a'].search(word, i + 1);
     }
     
     public boolean startsWith(String prefix) {
-        return startsWith(root, 0, prefix);
+        return startsWith(prefix, 0);
+    }
+
+    private boolean startsWith(String word, int i) {
+        if (word.length() == i) {
+            return true;
+        }
+
+        char c = word.charAt(i);
+
+        if (children[c - 'a'] == null) {
+            return false;
+        }
+
+        return children[c - 'a'].startsWith(word, i + 1);
     }
     
-    private boolean startsWith(TrieNode root, int i, String prefix){            
-        if(i >= prefix.length())
-            return true;
-        
-        int childIdx = prefix.charAt(i) - 'a';
-        if(root.children[childIdx] == null)
-            return false;
-        
-        return startsWith(root.children[childIdx], i + 1, prefix);        
-    }
 }
-
-/**
- * Your Trie object will be instantiated and called as such:
- * Trie obj = new Trie();
- * obj.insert(word);
- * boolean param_2 = obj.search(word);
- * boolean param_3 = obj.startsWith(prefix);
- */
