ca617e2299a80e2c024f7ff9ce26e2929aa47e55
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-03-27 11:35:08 +0530

    Time: 2 ms (76.27%), Space: 45.3 MB (42.56%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,13 +1,33 @@
 class Solution {
     public int minPathSum(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
         
-        int[][] minSum = new int[grid.length][grid[0].length];
-        for(int[] row : minSum)
-            Arrays.fill(row, -1);
+        int[][] minSum = new int[n][m];
         
-        return solve(0, 0, grid, minSum);
+        // tabulation
+        
+        // bottom-right cell
+        minSum[n - 1][m - 1] = grid[n - 1][m - 1]; 
+        
+        // right-most row
+        for(int i = n - 2; i >= 0; --i)
+            minSum[i][m - 1] = grid[i][m - 1] + minSum[i + 1][m - 1];
+        
+        // bottom-most column
+        for(int j = m - 2; j >= 0; --j)
+            minSum[n - 1][j] = grid[n - 1][j] + minSum[n - 1][j + 1];
+        
+        // remaining grid
+        for(int i = n - 2; i >= 0; --i){
+            for(int j = m - 2; j >= 0; --j)
+                minSum[i][j] = grid[i][j] + Math.min(minSum[i + 1][j], minSum[i][j + 1]);
+        }
+        
+        return minSum[0][0];
     }
     
+    // memoization
     public int solve(int i, int j, int[][] grid, int[][] minSum){
         if(i == grid.length - 1 && j == grid[0].length - 1)
             return grid[i][j];
