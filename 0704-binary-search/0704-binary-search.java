429f53ce4b9dbd88cb2aaa4c4be2e3a2e0468d24
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-04-01 13:09:49 +0530

    Time: 0 ms (100.00%), Space: 43.7 MB (11.09%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int si = 0, ei = nums.length - 1;
+        while(si <= ei){
+            int mid = (si + ei) / 2;
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid] < target)
+                si = mid + 1;
+            else
+                ei = mid - 1;                
+        }
+        
+        return -1;
+    }
+}
