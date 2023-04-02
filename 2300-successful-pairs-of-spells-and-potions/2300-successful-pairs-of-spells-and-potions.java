a8451785a36a8f698a63f01fb67d2ba583aa3d27
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-04-03 00:30:27 +0530

    Time: 119 ms (12.40%), Space: 63 MB (23.14%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.java
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.java
@@ -1,43 +1,43 @@
+
 class Solution {
-    // Our implementation of lower bound method using binary search.
-    private int lowerBound(int[] arr, int key) {
-        int lo = 0;
-        int hi = arr.length;
-        while (lo < hi) {
-            int mid = lo + (hi - lo) / 2;
-            if (arr[mid] < key) {
-                lo = mid + 1;
-            } else {
-                hi = mid;
-            }
-        }
-        return lo;
-    }
-    
     public int[] successfulPairs(int[] spells, int[] potions, long success) {
-        // Sort the potions array in increasing order.
+        
         Arrays.sort(potions);
-        int[] answer = new int[spells.length];
-
-        int m = potions.length;
-        int maxPotion = potions[m - 1];
-
-        for (int i = 0; i < spells.length; i++) {
-            int spell = spells[i];
-            // Minimum value of potion whose product with current spell  
-            // will be at least success or more.
-            long minPotion = (long) Math.ceil((1.0 * success) / spell);
-            // Check if we don't have any potion which can be used.
-            if (minPotion > maxPotion) {
-                answer[i] = 0;
-                continue;
-            }
-            // We can use the found potion, and all potion in its right 
-            // (as the right potions are greater than the found potion).
-            int index = lowerBound(potions, (int) minPotion);
-            answer[i] = m - index;
+        int[] result = new int[spells.length];
+        int i = 0;
+        for (int spell : spells) {
+            int ans1 = bSearch(potions, spell, success);
+            if (ans1 == -1)
+                result[i++] = 0;
+            else
+                result[i++] = potions.length - bSearch(potions, spell, success);
         }
+        return result;
+    }
 
-        return answer;
+    public static int bSearch(int[] arr, int spell, long tar) {
+        int i = 0, j = arr.length - 1;
+        int mid = (i + j) / 2;
+        boolean done = false;
+        while (i <= j) {
+            if (i == j) {
+                long p = (long) spell * (long) arr[i];
+                if (p >= tar)
+                    done = true;
+                break;
+            }
+            mid = (i + j) / 2;
+            long p = (long) spell * (long) arr[mid];
+            if (p < tar)
+                i = mid + 1;
+            else {
+                done = true;
+                j = mid;
+            }
+        }
+        if (!done)
+            return -1;
+
+        return i;
     }
 }
