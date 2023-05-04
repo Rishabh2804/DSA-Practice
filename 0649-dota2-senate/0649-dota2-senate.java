ce9c68716d09fedd01a59ccecbf50488568f8ff9
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-05-05 01:00:41 +0530

    Time: 13 ms (29.55%), Space: 43.3 MB (39.20%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.java
+++ 0649-dota2-senate/0649-dota2-senate.java
@@ -2,28 +2,21 @@ class Solution {
     public String predictPartyVictory(String senate) {
         int votes = 0;
         
-        int r = 0, d = 0;
         Queue<Character> senators = new LinkedList<>();
         for(int i = 0; i < senate.length(); ++i) {
-            senators.add(senate.charAt(i));
-            if(senate.charAt(i) == 'R') r++;
-            else d++;
+            senators.add(senate.charAt(i));            
         }
         
-        while(r > 0 && d > 0){
+        while(-1* senate.length() <= votes && votes <= senate.length()){
             char curr = senators.poll();
             if(curr == 'R'){
                 if(votes >= 0)
                     senators.add(curr);
-                else
-                    r--;
                 
                 votes++;                    
             } else{
                 if(votes <= 0)
                     senators.add(curr);
-                else
-                    d--;
                 
                 votes--;                    
             }
