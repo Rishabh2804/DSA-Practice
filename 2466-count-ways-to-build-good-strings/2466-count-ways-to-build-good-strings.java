e5d9abc44ba0feea8a286ea9ece161ed29eb3d3a
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-05-13 14:37:29 +0530

    Time: 11 ms (57.52%), Space: 41.4 MB (84.07%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.java
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.java
@@ -2,19 +2,19 @@ class Solution {
     static final int mod = 1000000007;
     
     // memoization
-    public int solve(int zero, int one, int n, int[] dp){
-        if(n < 0) return 0;
-        if(n == 0) return 1;
+//     public int solve(int zero, int one, int n, int[] dp){
+//         if(n < 0) return 0;
+//         if(n == 0) return 1;
         
-        if(dp[n] != -1) return dp[n] % mod;
+//         if(dp[n] != -1) return dp[n] % mod;
         
-        int ans0 = solve(zero, one, n - zero, dp) % mod;
-        int ans1 = solve(zero, one, n - one, dp) % mod;
+//         int ans0 = solve(zero, one, n - zero, dp) % mod;
+//         int ans1 = solve(zero, one, n - one, dp) % mod;
        
-        solve(zero, one, n - 1, dp);
+//         solve(zero, one, n - 1, dp);
         
-        return dp[n] = ((ans0 % mod) + (ans1 % mod)) % mod;
-    }
+//         return dp[n] = ((ans0 % mod) + (ans1 % mod)) % mod;
+//     }
     
     public int countGoodStrings(int low, int high, int zero, int one) {
         int[] dp = new int[high + 1];
