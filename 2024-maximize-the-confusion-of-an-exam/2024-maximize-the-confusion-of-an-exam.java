0902acaf8a1c2b3b09b29082bdb72f68f27e01ce
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-07-08 04:04:10 +0530

    Time: 13 ms (88.65%), Space: 44.3 MB (11.96%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
@@ -3,17 +3,12 @@ class Solution {
     public int maxConsecutiveAnswers(String answerKey, int k) {
         int n = answerKey.length();
         int[] prefix1 = new int[n + 1];
-        int[] prefix2 = new int[n + 1];
         
         for(int i = 1; i <= n; ++i){
-            if(answerKey.charAt(i - 1) == 'T'){
+            if(answerKey.charAt(i - 1) == 'T')
                 prefix1[i] = 1 + prefix1[i - 1];  
-                prefix2[i] = prefix2[i - 1];  
-            }
-            else{
+            else
                 prefix1[i] = prefix1[i - 1];  
-                prefix2[i] = 1 + prefix2[i - 1];  
-            }
         }
         
         int si = 0, ei = n + 1;
