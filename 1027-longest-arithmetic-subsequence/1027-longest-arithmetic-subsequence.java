0561230c0bcb2a9b1524cdc2677e265a1002a96a
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-07-15 00:12:05 +0530

    Time: 849 ms (10.39%), Space: 43.7 MB (95.48%) - LeetHub

--- 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.java
+++ 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.java
@@ -1,29 +1,4 @@
 class Solution {
-    static final int MAX_DIFF = 502;
-    static final int NULL_VAL = MAX_DIFF - 1;
-    
-    public int solve(int i, int d, int lastIdx, int[] nums, int[][] dp){
-        if(i >= nums.length) return 0;
-        
-        if(dp[i][lastIdx] != -1) return dp[i][lastIdx];
-
-        // we always have the provision to start a new AP from ith element
-        int take_new = solve(i + 1, NULL_VAL, i, nums, dp);
-
-        int leave = 0;
-        int take_forward = 0;
-        
-        // if the current element fits into ongoing AP, 
-        // then there is no point in leaving it
-
-        // * <d != NULL_VAL> condition gets checked automatically
-        if(lastIdx != NULL_VAL && nums[i] - nums[lastIdx] == d) 
-            take_forward = solve(i + 1, d, i, nums, dp);
-        else 
-            leave = solve(i + 1, d, lastIdx, nums, dp);
-        
-        return dp[i][lastIdx] = Math.max(leave, 1 + Math.max(take_forward, take_new));        
-    }
     
     public int longestSubsequence(int[] arr, int difference) {        
         int maxLen = 0;
