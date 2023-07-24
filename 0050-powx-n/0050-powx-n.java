d023b772bb3d28962483ef5600ede3810e69af4c
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-07-24 18:30:53 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (29.42%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,27 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n == 0) return 1.0;
+        if(x == 0 || x == 1) return x;
+        if(x == -1) {
+            if(n % 2 == 0) return 1;
+            else return -1;
+            
+            // return 1 - 2 * (n % 2);
+        }
+        
+        if(n == 1) return x;        
+        if(n == -1) return 1.0 / x;
+        
+        // if(n < 0) return myPow(1.0 / x, -1 * n);
+        
+        double ans = myPow(x, n / 2);
+        ans *= ans; // squaring the half-powered ans
+        
+        if(n % 2 == 1 || n % 2 == -1) {
+            if(n > 0) ans *= x;   
+            else ans /= x;
+        }
+        
+        return ans;
+    }
+}
