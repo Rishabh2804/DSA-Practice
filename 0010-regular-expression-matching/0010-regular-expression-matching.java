a23c7f7be784c5a800e82243592f19ec9eaad093
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-07-25 14:32:54 +0530

    Time: 1 ms (100.00%), Space: 41.4 MB (32.05%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -2,14 +2,15 @@ class Solution {
     static final char ANY_CHAR = '.';
     static final char ANY_FREQ = '*';
     
-    boolean isStarState(int pi, String p){
-        return pi + 1 < p.length() && p.charAt(pi + 1) == ANY_FREQ;
-    }
+//     boolean isStarState(int pi, String p){
+//         // check for star state
+//         return pi + 1 < p.length() && p.charAt(pi + 1) == ANY_FREQ;
+//     }
     
-    boolean isMatch(int si, int pi, String s, String p){
-        // check for match (identical character or dot state)
-        return p.charAt(pi) == ANY_CHAR || (si < s.length() && p.charAt(pi) == s.charAt(si));
-    }
+//     boolean isMatch(int si, int pi, String s, String p){
+//         // check for match (identical character or dot state)
+//         return p.charAt(pi) == ANY_CHAR || (si < s.length() && p.charAt(pi) == s.charAt(si));
+//     }
     
     Boolean solve(int si, int pi, String s, String p, Boolean[][] dp){
         if(pi >= p.length()) return si >= s.length();
@@ -17,18 +18,20 @@ class Solution {
         if(dp[si][pi] != null) return dp[si][pi];
         
         if(si >= s.length()){
-            if(isStarState(pi, p)) return dp[si][pi] = solve(si, pi + 2, s, p, dp);
+            // skippable if pattern reperesents star state
+            if(pi + 1 < p.length() && p.charAt(pi + 1) == ANY_FREQ) 
+                return dp[si][pi] = solve(si, pi + 2, s, p, dp);
             else return dp[si][pi] = false;
         }
         
         // check for star state
-        if(isStarState(pi, p)) {
+        if(pi + 1 < p.length() && p.charAt(pi + 1) == ANY_FREQ) {
             // option to skip current pattern state
             boolean skip = solve(si, pi + 2, s, p, dp);  
             if(skip == true) return dp[si][pi] = true;
             
             // check for match
-            if(isMatch(si, pi, s, p)) {
+            if(p.charAt(pi) == ANY_CHAR || (si < s.length() && p.charAt(pi) == s.charAt(si))) {
                 boolean repeat = solve(si + 1, pi, s, p, dp);                
                 if(repeat == true) return dp[si][pi] = true;
                 else {
@@ -38,7 +41,7 @@ class Solution {
             } else return dp[si][pi] = skip; // would be false anyways                        
         }
         
-        if(isMatch(si, pi, s, p)) 
+        if(p.charAt(pi) == ANY_CHAR || (si < s.length() && p.charAt(pi) == s.charAt(si))) 
             return dp[si][pi] = solve(si + 1, pi + 1, s, p, dp);
         
         return dp[si][pi] = false;
