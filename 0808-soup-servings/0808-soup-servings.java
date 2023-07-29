1346624c1a4ffaa176bb46c8a5baf552f8a0e258
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-07-29 13:20:22 +0530

    Time: 5 ms (43.33%), Space: 43 MB (43.33%) - LeetHub

---
+++ 0808-soup-servings/0808-soup-servings.java
@@ -0,0 +1,32 @@
+class Solution {
+    
+    static final int[] stateA = {4, 3, 2, 1};
+    static final int[] stateB = {0, 1, 2, 3};
+    static final double BRANCH_PROB = 0.25;
+    static final int LIMIT = 200;
+    
+    public double solve(int a, int b, double[][] dp){
+        if(a <= 0 && b <= 0) return 0.5; 
+        else if(a <= 0) return 1.0;
+        else if(b <= 0) return 0.0;
+        
+        if(dp[a][b] != -1) return dp[a][b];
+        
+        double prob = 0.0;
+        for(int i = 0; i < 4; ++i)
+            prob += solve(a - stateA[i], b - stateB[i], dp) * BRANCH_PROB;
+        
+        return dp[a][b] = prob;
+    }
+    
+    public double soupServings(int n) {        
+        n = (n + 25 - 1) / 25; // ceil divide
+        n = Math.min(n, LIMIT);
+        
+        double[][] dp = new double[n + 1][n + 1];
+        for(double[] row : dp)
+            Arrays.fill(row, -1);
+        
+        return solve(n, n, dp);
+    }
+}
