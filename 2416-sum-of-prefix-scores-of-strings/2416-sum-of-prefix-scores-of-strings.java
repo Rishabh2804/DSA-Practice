15f9d527b45b601e82190803a97a5cd0a7540b33
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-08-02 20:34:55 +0530

    Time: 568 ms (67.83%), Space: 183.2 MB (97.90%) - LeetHub

---
+++ 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.java
@@ -0,0 +1,95 @@
+class TrieNode {
+    TrieNode[] children;
+    boolean isEndOfWord;
+    int words;
+    
+    public TrieNode(){
+        children = new TrieNode[26];
+        isEndOfWord = false;
+        words = 0;
+    }
+}
+
+class Trie {
+    TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    private void add(TrieNode root, String key, int si) {
+        if (si >= key.length()) {
+            root.isEndOfWord = true;
+            return;
+        }
+
+        int childIndex = key.charAt(si) - 'a';
+        TrieNode child = root.children[childIndex];
+        if (child == null) {
+            child = new TrieNode();
+            root.children[childIndex] = child;
+        }
+
+        child.words++;
+
+        add(child, key, si + 1);
+    }
+
+    public void add(String word) {
+        add(this.root, word, 0);
+    }
+    
+    // analogous to creating prefix sum array out of normal array
+    private void transformTrie(TrieNode root, int prevScore){
+        if(root == null) return;
+        
+        // if(root.isEndOfWord){
+            root.words += prevScore;
+            prevScore = root.words;
+        // }
+        
+        for(TrieNode child : root.children){
+            if (child != null) 
+                transformTrie(child, prevScore);
+        }        
+    }
+    
+    public void transformTrie(){
+        transformTrie(root, 0);        
+    }
+    
+    // search for a word, and return its 'words' value
+    private int getPrefixCount(TrieNode root, String key, int si) {
+        if (si >= key.length()) 
+            return root.words;
+
+        int childIndex = key.charAt(si) - 'a';
+        TrieNode child = root.children[childIndex];
+        if (child == null)
+            return 0;
+        else 
+            return getPrefixCount(child, key, si + 1);        
+    }
+
+    public int getPrefixCount(String word) {
+        return getPrefixCount(root, word, 0);
+    }
+}
+
+class Solution {
+    public int[] sumPrefixScores(String[] words) {
+        
+        Trie trie = new Trie();
+        for(String word : words)
+            trie.add(word);
+        
+        trie.transformTrie();
+        
+        int[] scores = new int[words.length];
+        int i = 0;        
+        for(String word : words)
+            scores[i++] = trie.getPrefixCount(word);
+        
+        return scores;
+    }
+}
