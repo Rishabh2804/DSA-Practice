0da9db1f246bae7f80d2708bdd027ee681b7ec0d
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-08-05 01:05:57 +0530

    Time: 0 ms (100.00%), Space: 44 MB (85.38%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    
+    static int xorN(int n){
+        return switch(n % 4){
+            case 0 -> n;
+            case 1 -> 1;
+            case 2 -> n + 1;
+            default -> 0;
+        };
+    }
+    
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int xorAll = xorN(n);
+        
+        for(int i : nums)
+            xorAll ^= i;
+        
+        return xorAll;
+    }
+}
