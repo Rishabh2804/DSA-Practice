6fc8828fd5f9d6232d2c64b4234f97ab09b555b4
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-08-07 17:53:49 +0530

    Time: 843 ms (5.40%), Space: 49.5 MB (46.56%) - LeetHub

---
+++ 0089-gray-code/0089-gray-code.java
@@ -0,0 +1,48 @@
+class Solution {
+    public List<Integer> grayCode(int n) {
+        
+        int maxVal = (1 << n) - 1;
+        List<Integer> codes = new LinkedList<>();        
+        codes.add(0);
+        
+        int bitPos = 0;
+        while(bitPos < n){
+            int mask = 1 << bitPos;
+            
+            int size = codes.size();
+            for(int i = size - 1; i >= 0; --i){
+                int grayCode = codes.get(i) | mask;
+                codes.add(grayCode);
+            }
+            
+            bitPos++;
+        }
+        
+        return codes;
+    }
+}
+
+// 111
+// 011
+// 001
+// 000
+// 010
+// 110
+// 100
+// 101
+
+
+// 000 100 110 010 011 111 101 001
+
+// 111
+// 011 111
+// 001 101 111 011
+// 000 100 110 010 011 111 101 001
+
+// 111
+// 111 011
+// 111 011 001 101
+// 111 011 001 101 100 000 010 110
+
+
+// 0111 1111 1111 0111
