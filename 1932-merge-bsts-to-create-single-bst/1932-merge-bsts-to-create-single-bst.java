2154b57df181015caf9b1eafd88adfbf76f2e025
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-08-10 00:45:30 +0530

    Time: 63 ms (29.41%), Space: 70.2 MB (19.61%) - LeetHub

---
+++ 1932-merge-bsts-to-create-single-bst/1932-merge-bsts-to-create-single-bst.java
@@ -0,0 +1,122 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    static class Box{
+        int low;
+        int high;
+        boolean isBST;
+        
+        Box(int low, int high, boolean isBST){
+            this.low = low;
+            this.high = high;
+            this.isBST = isBST;
+        }
+    }
+    
+    private Box isBST(TreeNode root){
+        if(root == null) return new Box(Integer.MAX_VALUE, Integer.MIN_VALUE, true);
+        
+        Box curr = new Box(root.val, root.val, false);
+        
+        Box left = isBST(root.left);
+        if(!left.isBST || left.high >= root.val) return curr;
+        else curr.low = Math.min(curr.low, left.low);
+        
+        Box right = isBST(root.right);
+        if(!right.isBST || right.low <= root.val) return curr;
+        else curr.high = Math.max(curr.high, right.high);
+        
+        curr.isBST = true;
+        return curr;
+    }
+    
+    void dfs(TreeNode root, int i, List<TreeNode> trees, HashMap<Integer, Integer> nodes){
+        if(root == null) return;
+            
+        // checking leaf node
+        if(root.left == null && root.right == null) {
+            Integer idx = nodes.get(root.val);
+            if(idx == null) return;
+            
+            TreeNode rep = trees.get(idx);
+            root.left = rep.left;
+            root.right = rep.right;
+            
+            nodes.remove(root.val);
+            trees.set(idx, null);
+            
+            if(idx < i) return; // would have been already traversed
+        }
+        
+        dfs(root.left, i, trees, nodes);
+        dfs(root.right, i, trees, nodes);
+    }
+    
+    public TreeNode canMerge(List<TreeNode> trees) {
+        HashMap<Integer, TreeNode> parent = new HashMap<>();
+        HashMap<Integer, Integer> nodes = new HashMap<>();
+        
+        int k = 0;
+        for(TreeNode root : trees){
+            if(root.left != null)
+                parent.put(root.left.val, root);
+            
+            if(root.right != null)
+                parent.put(root.right.val, root); 
+            
+            nodes.put(root.val, k++);
+        }
+        
+        TreeNode ans = null;
+//         for(TreeNode root : trees){
+//             TreeNode parNode = parent.get(root.val);
+//             if(parNode == null){
+//                 if(ans == null) {
+//                     ans = root;
+//                     continue;
+//                 } else return null;
+//             }
+            
+//             if(parNode.val > root.val) 
+//                 parNode.left = root;
+//             else 
+//                 parNode.right = root;
+            
+//             parent.remove(root.val);
+//         }
+        
+        for(int i = 0; i < trees.size(); ++i){   
+            if(trees.get(i) == null) continue;
+            
+            // temporarily marking visited
+            TreeNode curr = trees.get(i);
+            // trees.set(i, null);
+            
+            nodes.remove(curr.val);
+            
+            dfs(curr, i, trees, nodes);
+            ans = trees.get(i);                        
+            
+            nodes.put(curr.val, i);
+        }
+        
+        if(nodes.size() > 1) return null;        
+        
+        if(isBST(ans).isBST) return ans;
+        else return null;
+    }
+}
