7d05bd7abb45d6fe5b1624e55204be8910da9959
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-08-13 02:35:43 +0530

    Time: 8 ms (19.21%), Space: 40.8 MB (13.57%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.java
@@ -0,0 +1,48 @@
+class Solution {
+        
+    static final int SIZE = 10;
+    static int[] factorial = new int[SIZE];
+    
+    private static void fact(int n){        
+        factorial[0] = 1;        
+        for(int i = 1; i <= n; ++i)
+            factorial[i] = i * factorial[i - 1];
+    } 
+    
+    public ArrayList<Integer> solve(int n, int k){ 
+        if(n == 1) {
+            ArrayList<Integer> ans = new ArrayList<>();
+            ans.add(1);
+            return ans;
+        }
+        
+        int unit = factorial[n - 1];        
+        
+        int start = 1 + (k / unit);
+        int k1 = k % unit;
+        if(k1 == 0){
+            start--;
+            k1 = unit;
+        }
+        
+        ArrayList<Integer> ans = solve(n - 1, k1);
+        for(int i = 0; i < n - 1; ++i) {
+            if(ans.get(i) >= start)
+                ans.set(i, ans.get(i) + 1);                        
+        }
+        
+        ans.add(start);
+        return ans;
+    }
+    
+    public String getPermutation(int n, int k) {
+        fact(n);
+        
+        ArrayList<Integer> ans = solve(n, k);
+        String res = "";
+        for(int i = ans.size() - 1; i >= 0; --i)
+            res += (char)(ans.get(i) + '0');
+        
+        return res;
+    }
+}
