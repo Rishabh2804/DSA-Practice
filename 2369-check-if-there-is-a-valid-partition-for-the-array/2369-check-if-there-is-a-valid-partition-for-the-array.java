a846019eca711bd9f08a3ea03d0fd62ded7a419d
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-08-14 02:05:55 +0530

    Time: 8 ms (78.57%), Space: 56.8 MB (63.27%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.java
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.java
@@ -1,26 +1,4 @@
 class Solution {
-    public boolean solve(int i, int[] nums, Boolean[] dp){        
-        if(i == nums.length - 1) return false;
-        if(i >= nums.length) return true;        
-        
-        if(dp[i] != null) return dp[i];
-        
-        boolean ans = false;
-                
-        if(i <= nums.length - 3) {
-            if(nums[i] == nums[i + 1] && nums[i + 1] == nums[i + 2])
-                ans = solve(i + 3, nums, dp);              
-            else if((1 + nums[i]) == nums[i + 1] && (1 + nums[i + 1]) == nums[i + 2]) 
-                ans = solve(i + 3, nums, dp);
-        } 
-        
-        if(ans) return dp[i] = ans;
-        
-        if(nums[i] == nums[i + 1])
-            ans = solve(i + 2, nums, dp);
-            
-        return dp[i] = ans;
-    }
     
     public boolean validPartition(int[] nums) {
         
