a740cf68af8907e751c59794e00aed754033537a
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-08-14 02:05:41 +0530

    Time: 8 ms (78.57%), Space: 56.5 MB (78.57%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.java
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.java
@@ -23,6 +23,34 @@ class Solution {
     }
     
     public boolean validPartition(int[] nums) {
-        return solve(0, nums, new Boolean[nums.length]);
+        
+        int n = nums.length;        
+        boolean[] dp = new boolean[n];
+        // dp[0] = true;
+        
+        for(int i = 1; i < nums.length; ++i){
+            if(i >= 2){
+                // three equal elements
+                if(nums[i] == nums[i - 1] && nums[i - 1] == nums[i - 2]){
+                    if(i == 2) dp[i] = true;
+                    else dp[i] = dp[i - 3];
+                }
+                // three consecutive elements
+                else if((nums[i] - 1) == nums[i - 1] && (nums[i - 1] - 1) == nums[i - 2]){
+                    if(i == 2) dp[i] = true;
+                    else dp[i] = dp[i - 3];
+                }
+            }
+            
+            if(dp[i]) continue;
+            
+            // two equal elements
+            if(nums[i] == nums[i - 1]){
+                if(i == 1) dp[i] = true;
+                else dp[i] = dp[i - 2];
+            }
+        }
+        
+        return dp[n - 1];
     }
 }
