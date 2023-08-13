20f64045a196c629bae8c3b6e850b3fb7281f006
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-08-14 02:07:17 +0530

    Time: 7 ms (85.71%), Space: 59.7 MB (45.92%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.java
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.java
@@ -1,31 +1,28 @@
 class Solution {
+    public boolean solve(int i, int[] nums, Boolean[] dp){        
+        if(i == nums.length - 1) return false;
+        if(i >= nums.length) return true;        
+        
+        if(dp[i] != null) return dp[i];
+        
+        boolean ans = false;
+                
+        if(i <= nums.length - 3) {
+            if(nums[i] == nums[i + 1] && nums[i + 1] == nums[i + 2])
+                ans = solve(i + 3, nums, dp);              
+            else if((1 + nums[i]) == nums[i + 1] && (1 + nums[i + 1]) == nums[i + 2]) 
+                ans = solve(i + 3, nums, dp);
+        } 
+        
+        if(ans) return dp[i] = ans;
+        
+        if(nums[i] == nums[i + 1])
+            ans = solve(i + 2, nums, dp);
+            
+        return dp[i] = ans;
+    }
     
     public boolean validPartition(int[] nums) {
-        
-        int n = nums.length;        
-        boolean[] dp = new boolean[n];
-        
-        for(int i = 1; i < nums.length; ++i){
-            if(i >= 2){
-                // three equal elements
-                if(nums[i] == nums[i - 1] && nums[i - 1] == nums[i - 2]){
-                    if(i == 2) dp[i] = true;
-                    else dp[i] = dp[i - 3];
-                }
-                // three consecutive elements
-                else if((nums[i] - 1) == nums[i - 1] && (nums[i - 1] - 1) == nums[i - 2]){
-                    if(i == 2) dp[i] = true;
-                    else dp[i] = dp[i - 3];
-                }
-            }
-            
-            // two equal elements
-            if(nums[i] == nums[i - 1]){
-                if(i == 1) dp[i] = true;
-                else dp[i] |= dp[i - 2];
-            }
-        }
-        
-        return dp[n - 1];
+        return solve(0, nums, new Boolean[nums.length]);
     }
 }
