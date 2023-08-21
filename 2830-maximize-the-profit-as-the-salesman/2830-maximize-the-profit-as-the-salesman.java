51785d51835f2bcdd452207282554e9adc341aad
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-08-22 02:14:54 +0530

    Time: 216 ms (14.85%), Space: 125.8 MB (8.58%) - LeetHub

--- 2830-maximize-the-profit-as-the-salesman/2830-maximize-the-profit-as-the-salesman.java
+++ 2830-maximize-the-profit-as-the-salesman/2830-maximize-the-profit-as-the-salesman.java
@@ -16,19 +16,25 @@ class Solution {
         return si;
     }
     
-    public int solve(int i, List<List<Integer>> offers, int[] dp){
+    public int solve(int i, int si, int ei, List<List<Integer>> offers, int[] dp){
+        // if(i >= offers.size() || si >= ei) return 0;
         if(i >= offers.size()) return 0;
         
         if(dp[i] != -1) return dp[i];
         
-        int leave = solve(i + 1, offers, dp);
-                
+        int leave = solve(i + 1, si, ei, offers, dp);
+
         int take = offers.get(i).get(GOLD);
         int nextIdx = lowerBound(i + 1, offers.get(i).get(END), offers);
         if(nextIdx < offers.size())
-            take += solve(nextIdx, offers, dp);
+            take += solve(nextIdx, 1 + offers.get(i).get(END), ei, offers, dp);
         
         return dp[i] = Math.max(leave, take);
+        
+        
+        // int take = offers.get(i).get(GOLD) + solve(nextIdx, 1 + offers.get(i).get(END), ei, offers, dp);
+        
+        // return dp[si] = Math.max(leave, take);
     }
     
     public int maximizeTheProfit(int n, List<List<Integer>> offers) {
@@ -37,7 +43,7 @@ class Solution {
         int[] dp = new int[offers.size()];
         Arrays.fill(dp, -1);
 
-        int ans = solve(0, offers, dp);
+        int ans = solve(0, 0, n - 1, offers, dp);
         
         return ans;
     }
