88909a707b48757d88f03aab6838415810a79481
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-08-22 14:34:09 +0530

    Time: 478 ms (62.41%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,11 +1,12 @@
 # Write your MySQL query statement below
 
-# with SortedSalaries as (
-#     select distinct salary 
-#     from Employee
-#     order by salary desc
-#     limit 2
-# )
+with SortedSalaries as (
+    select distinct salary 
+    from Employee
+    order by salary desc
+    limit 1
+    offset 1
+)
 
 select distinct (
     select distinct salary 
