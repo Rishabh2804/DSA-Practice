2e54800d92dbad037d67e9991fc3a409f9be27ef
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-08-25 00:52:10 +0530

    Time: 3695 ms (10.99%), Space: 0B (100.00%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -3,16 +3,16 @@
 with TopUser as (
     select U.name as results,
         row_number() over (order by count(R.user_id) desc, U.name) as rn    
-    from Users U join MovieRating R
+    from Users U inner join MovieRating R
     on U.user_id = R.user_id
     group by U.user_id
 ),
 TopMovie as (
     select title as results,
         Row_Number() over (order by avg(rating) desc, M.title) as rn    
-    from Movies M join MovieRating R
+    from Movies M inner join MovieRating R
     on M.movie_id = R.movie_id 
-    where created_at between '2020-02-01' and '2020-02-29'
+    where month(created_at) = '02' and year(created_at) = '2020'
     group by M.title
 )
 select results
