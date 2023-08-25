6dfa56660c9efbcbeb8195352e5e457accf10616
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-08-26 00:19:23 +0530

    Time: 1 ms (91.57%), Space: 40.9 MB (42.67%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,24 +1,38 @@
 class Solution {
-    private boolean solve(int i, int j, int k, String s1, String s2, String s3, Boolean[][][] dp){
-        if(k >= s3.length()) return i >= s1.length() && j >= s2.length();
+    private int solve(int i, int j, int k, String s1, String s2, String s3, Integer[][] dp){
+        if(k >= s3.length()) return 0;
         
-        if(i >= s1.length()) return s2.substring(j).equals(s3.substring(k));
-        if(j >= s2.length()) return s1.substring(i).equals(s3.substring(k));
+        if(i >= s1.length()){
+            if(s2.substring(j).equals(s3.substring(k)))
+                return s3.length() - k;
+            else return 0;
+        }
         
-        if(dp[i][j][k] != null) return dp[i][j][k];
+        if(j >= s2.length()){
+            if(s1.substring(i).equals(s3.substring(k)))
+                return s3.length() - k;
+            else return 0;
+        }
         
-        boolean ans = false;
+        if(dp[i][j] != null) return dp[i][j];
+        
+        int op1 = 0;
         if(s1.charAt(i) == s3.charAt(k))
-            ans = ans || solve(i + 1, j, k + 1, s1, s2, s3, dp);
+            op1 = 1 + solve(i + 1, j, k + 1, s1, s2, s3, dp);
         
+        int op2 = 0;
         if(s2.charAt(j) == s3.charAt(k))
-            ans = ans || solve(i, j + 1, k + 1, s1, s2, s3, dp);
+            op2 = 1 + solve(i, j + 1, k + 1, s1, s2, s3, dp);
         
-        return dp[i][j][k] = ans; 
+        return dp[i][j] = Math.max(op1, op2); 
     }
     
     public boolean isInterleave(String s1, String s2, String s3) {
-        Boolean[][][] dp = new Boolean[s1.length()][s2.length()][s3.length()];
-        return solve(0, 0, 0, s1, s2, s3, dp);
+        if(s3.length() != s1.length() + s2.length()) return false;
+        
+        Integer[][] dp = new Integer[s1.length()][s2.length()];
+        int match = solve(0, 0, 0, s1, s2, s3, dp);
+        
+        return match == s3.length();
     }
 }
