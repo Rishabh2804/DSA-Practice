5b6efb905921e9607e8e6950b7d59f165d5455eb
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-08-29 02:16:25 +0530

    Time: 28 ms (8.39%), Space: 41.1 MB (9.49%) - LeetHub

--- 1444-number-of-ways-of-cutting-a-pizza/1444-number-of-ways-of-cutting-a-pizza.java
+++ 1444-number-of-ways-of-cutting-a-pizza/1444-number-of-ways-of-cutting-a-pizza.java
@@ -1,29 +1,85 @@
 class Solution {
-    public int ways(String[] pizza, int k) {
-        int m = pizza.length, n = pizza[0].length();
-        Integer[][][] dp = new Integer[k][m][n];
-        int[][] preSum = new int[m+1][n+1];
-
-        for (int r = m - 1; r >= 0; r--)
-            for (int c = n - 1; c >= 0; c--)
-                preSum[r][c] = preSum[r][c+1] + preSum[r+1][c] - preSum[r+1][c+1] + (pizza[r].charAt(c) == 'A' ? 1 : 0);
-
-        return dfs(m, n, k-1, 0, 0, dp, preSum);
+    
+    static final int MOD = 1000000007;
+    
+    static int[][] apples;
+    private int getApples(int r, int c){
+        if(r == 0)
+            return apples[apples.length - 1][c];
+        else return apples[apples.length - 1][c] - apples[r - 1][c];
     }
-
-    int dfs(int m, int n, int k, int r, int c, Integer[][][] dp, int[][] preSum) {
-        if (preSum[r][c] == 0) return 0; 
-        if (k == 0) return 1;
-        if (dp[k][r][c] != null) return dp[k][r][c];
-        int ans = 0;
+    
+    public int solve(int r, int c, int k, Integer[][][] dp){
+        if(k == 1) return 1;
         
-        for (int nr = r + 1; nr < m; nr++) 
-            if (preSum[r][c] - preSum[nr][c] > 0)
-                ans = (ans + dfs(m, n, k - 1, nr, c, dp, preSum)) % 1_000_000_007;
-        for (int nc = c + 1; nc < n; nc++) 
-            if (preSum[r][c] - preSum[r][nc] > 0)
-                ans = (ans + dfs(m, n, k - 1, r, nc, dp, preSum)) % 1_000_000_007;
+        if(dp[r][c][k] != null) return dp[r][c][k];
+        
+        int rows = dp.length;
+        int cols = dp[0].length;
+        
+        int totalApples = getApples(r, c);
+        
+        int ans = 0;
+        for(int i = r + 1; i < rows; ++i){
+            int apples1 = getApples(i, c);
+            int apples2 = totalApples - apples1;
+            
+            if(apples1 == 0 || apples2 == 0) continue;
+            
+            ans = ((ans % MOD) + (solve(i, c, k - 1, dp) % MOD)) % MOD;
+        }
+        
+        for(int i = c + 1; i < cols; ++i){
+            int apples1 = getApples(r, i);
+            int apples2 = totalApples - apples1;
+            
+            if(apples1 == 0 || apples2 == 0) continue;
+            
+            ans = ((ans % MOD) + (solve(r, i, k - 1, dp) % MOD)) % MOD;
+        }
+        
+        return dp[r][c][k] = ans;
+    }
+    public int ways(String[] pizza, int k) {
+        int rows = pizza.length;
+        int cols = pizza[0].length();
                 
-        return dp[k][r][c] = ans;
+        apples = new int[rows][cols];
+        apples[0][cols - 1] = (pizza[0].charAt(cols - 1) == 'A') ? 1 : 0;
+        
+        for(int col = cols - 2; col >= 0; --col){
+            apples[0][col] = apples[0][col + 1];
+            if(pizza[0].charAt(col) == 'A')
+                apples[0][col]++;
+        }
+        
+        for(int row = 1; row < rows; ++row){
+            apples[row][cols - 1] = apples[row - 1][cols - 1];
+            if(pizza[row].charAt(cols - 1) == 'A')
+                apples[row][cols - 1]++;
+        }
+        
+        for(int row = 1; row < rows; ++row){
+            for(int col = cols - 2; col >= 0; --col){
+                // inclusion - exclusion principle
+                apples[row][col] = apples[row][col + 1] + apples[row - 1][col] - apples[row - 1][col + 1];
+                if(pizza[row].charAt(col) == 'A')
+                    apples[row][col]++;                                
+            }
+        }
+                
+        if(k == 1){
+            if(apples[rows - 1][0] > 0) return 1;
+            else return 0;
+        }
+        
+        for(int[] row : apples){
+            for(int apple : row)
+                System.out.print(apple + " ");
+            System.out.println();
+        }
+        
+        Integer[][][] dp = new Integer[rows][cols][k + 1];
+        return solve(0, 0, k, dp);
     }
 }
