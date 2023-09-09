34e3ca40ff7da08f0c8600742729da3b2946e4fb
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-09-09 22:59:55 +0530

    Time: 3 ms (94.42%), Space: 43.4 MB (95.35%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,6 +1,6 @@
 class Solution {
     
-    private static final boolean canTakeCourse(int courseID, List<List<Integer>> adj, Boolean[] vis){
+    private boolean canTakeCourse(int courseID, List<List<Integer>> adj, Boolean[] vis){
         
         if(vis[courseID] != null) return !vis[courseID];
         
@@ -15,16 +15,17 @@ class Solution {
         return true;
     }
     
-    public final boolean canFinish(int numCourses, int[][] prerequisites) {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
         List<List<Integer>> adj = new ArrayList<>();
         for(int i = 0; i < numCourses; ++i)
             adj.add(new ArrayList<>());
         
+
         for(int[] edge : prerequisites){
             int u = edge[0];
             int v = edge[1];
             
-            adj.get(u).add(v);
+            adj.get(v).add(u);
         }
         
         Boolean[] vis = new Boolean[numCourses];
