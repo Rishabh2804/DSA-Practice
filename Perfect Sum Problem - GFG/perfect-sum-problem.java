341d237322a35330ef7d01e18265856aca039da6
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-09-14 01:48:27 +0530

    Added solution - LeetHub

--- Perfect Sum Problem - GFG/perfect-sum-problem.java
+++ Perfect Sum Problem - GFG/perfect-sum-problem.java
@@ -29,8 +29,8 @@ class Solution{
 
     static final int MOD = 1000000007;
     
-    public int solve(int i, int sum, List<Integer> arr, Integer[][] ways){
-        if(i >= arr.size()) {
+    public int solve(int i, int sum, int[] arr, Integer[][] ways){
+        if(i >= arr.length) {
             if(sum == 0) return 1;
             else return 0;
         }
@@ -40,19 +40,13 @@ class Solution{
         int leave = solve(i + 1, sum, arr, ways);
         
         int take = 0;
-        if(arr.get(i) <= sum) take = solve(i + 1, sum - arr.get(i), arr, ways);
+        if(arr[i] <= sum) take = solve(i + 1, sum - arr[i], arr, ways);
         
         return ways[i][sum] = (leave + take) % MOD;
     }
     
 	public int perfectSum(int arr[],int n, int sum) 
 	{ 
-	   // Arrays.sort(arr);
-	    
-	   List<Integer> list = new ArrayList<>();
-	   for(int i : arr)
-	       list.add(i);
-	   
 	   Integer[][] ways = new Integer[n][sum + 1];
 	   // for(int i = 0; i < n; ++i)
 	   //     ways[i][0] = 1;
@@ -74,6 +68,6 @@ class Solution{
 	    
 	   // return ways[n - 1][sum] % MOD;
 	   
-	   return solve(0, sum, list, ways);
+	   return solve(0, sum, arr, ways);
 	} 
 }
