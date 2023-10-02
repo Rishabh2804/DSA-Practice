4f02f57f1010f826d7adf4f845bf15f1527f7fc1
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-10-02 11:55:38 +0530

    Time: 9 ms (87.14%), Space: 44.1 MB (56.20%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.java
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.java
@@ -5,9 +5,9 @@ class Solution {
         int alice = 0;
         int bob = 0;
         
-        int count = 0;
+        int count = 1;
         char start = colors.charAt(0);
-        for(int i = 0; i < colors.length(); ++i){
+        for(int i = 1; i < colors.length(); ++i){
             char curr = colors.charAt(i);
             if(curr == start)
                 count++;
