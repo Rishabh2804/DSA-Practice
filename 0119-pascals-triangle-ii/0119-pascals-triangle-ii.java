ed551b6c2a7f417aa54b9fec3184f892446ec685
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-10-16 13:41:12 +0530

    Time: 2 ms (18.86%), Space: 40.3 MB (44.42%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    
+    void pascalise(ArrayList<Integer> row){
+        row.add(0);
+        for(int i = row.size() - 1; i > 0; --i)
+            row.set(i, row.get(i) + row.get(i - 1));
+    }
+    
+    public List<Integer> getRow(int rowIndex) {
+        ArrayList<Integer> row = new ArrayList<>();
+        row.add(1);
+        
+        if(rowIndex == 0) return row;
+        
+        while(rowIndex --> 0){
+            pascalise(row);
+        }
+        
+        return row;
+    }
+}
