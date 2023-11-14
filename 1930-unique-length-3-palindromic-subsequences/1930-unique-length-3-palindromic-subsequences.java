149aa1ccf6b117fda7fc5ddbee5b10bec69a0635
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-11-14 12:40:55 +0530

    Time: 170 ms (68.63%), Space: 44.9 MB (35.29%) - LeetHub

---
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.java
@@ -0,0 +1,37 @@
+class Solution {
+    static final int SI = 0;
+    static final int EI = 1;
+    
+    private int countDistinct(int si, int ei, String s){
+        HashSet<Character> dictionary = new HashSet<>();
+        for(int i = si; i < ei; ++i)
+            dictionary.add(s.charAt(i));
+        
+        return dictionary.size();
+    }
+    
+    public int countPalindromicSubsequence(String s) {
+        
+        int n = s.length();
+        
+        int count = 0;
+        
+        int[][] occurences = new int[26][2];        
+        for(int i = 0; i < 26; ++i)
+            occurences[i] = new int[]{n, -1};
+        
+        for(int i = 0; i < n; ++i){
+            int idx = s.charAt(i) - 'a';
+            
+            occurences[idx][SI] = Math.min(occurences[idx][SI], i);
+            occurences[idx][EI] = Math.max(occurences[idx][EI], i);            
+        }
+        
+        for(int[] occurence : occurences){
+            System.out.println(occurence[SI] + " " + occurence[EI]);
+            
+            count += countDistinct(occurence[SI] + 1, occurence[EI], s);
+        }
+        return count;
+    }
+}
