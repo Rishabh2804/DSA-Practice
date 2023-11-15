c8ed7aacc4ca778b58afce18fe188d4066c27c88
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-11-16 01:56:12 +0530

    Time: 6 ms (81.88%), Space: 56.7 MB (22.46%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.java
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.java
@@ -4,21 +4,19 @@ class Solution {
         int n = arr.length;
         Arrays.sort(arr);  
         
-        // int dec = arr[0] - 1;
-        int dec = 0;
-        arr[0] = 1;
+        int dec = arr[0] - 1;
         
         for(int i = 1; i < n; ++i){
             int effPrev = arr[i - 1] - dec;
-            int effCurr = arr[i] - dec;
+            int effCurr = arr[i];
             
-            if(effCurr - effPrev <= 1) continue;
+            if(effCurr - effPrev <= 1) {
+                dec = 0;
+                continue;
+            }
             
-            arr[i] = arr[i - 1] + 1;
-            // dec = arr[i] - effPrev - 1;            
+            dec = effCurr - effPrev - 1;            
         }
-        
-        // System.out.println(arr[n - 1] + " " + dec);
         
         return arr[n - 1] - dec;
     }
