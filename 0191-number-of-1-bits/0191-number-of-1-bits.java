4771488e8cee35f7d3a29d601c6b8028c805423a
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-11-29 20:04:07 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (28.07%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -1,9 +1,28 @@
 public class Solution {
     // you need to treat n as an unsigned value
-    public int hammingWeight(int n) {
-        if(n < 0) return 1 + hammingWeight(Integer.MIN_VALUE ^ n);
+    
+    // Recursive
+    private int solve1(int n){
+        if(n < 0) return 1 + solve1(Integer.MIN_VALUE ^ n);
         
         if(n == 0) return 0;
-        return (n & 1) + hammingWeight(n >> 1);
+        return (n & 1) + solve1(n >> 1);
+    }
+        
+    // Iterative
+    private int solve2(int n){
+        if(n < 0) return 1 + solve2(Integer.MIN_VALUE ^ n);
+        
+        int count = 0;
+        for(int bit = 0; bit < 31; ++bit){
+            boolean bitSet = (n & (1 << bit)) != 0;
+            if (bitSet) count++;
+        }
+
+        return count;
+    }
+    
+    public int hammingWeight(int n) {
+        return solve2(n);
     }
 }
