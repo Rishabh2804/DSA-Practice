7c9759ef783eed3776fe1849dc64b090d3a3ab77
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-12-23 13:16:21 +0530

    Time: 1 ms (96.48%), Space: 41.8 MB (9.03%) - LeetHub

---
+++ 1496-path-crossing/1496-path-crossing.java
@@ -0,0 +1,34 @@
+class Solution {
+    
+    static final int BASE = 10001;
+    
+    public boolean isPathCrossing(String path) {
+        int vertical = 0;
+        int horizon = 0;
+        
+        int currPos = BASE*vertical + horizon;
+        HashSet<Integer> coords = new HashSet<>();
+        coords.add(currPos);
+        
+        for(int i = 0; i < path.length(); ++i){
+            switch(path.charAt(i)){
+                case 'N' : vertical++; 
+                           break;
+                case 'E' : horizon++; 
+                           break;
+                case 'S' : vertical--; 
+                           break;
+                case 'W' : horizon--; 
+                           break;
+                default  : break;                
+            }
+            
+            currPos = BASE*vertical + horizon;
+            if(coords.contains(currPos)) return true;
+            
+            coords.add(currPos);
+        }
+        
+        return false;
+    }
+}
