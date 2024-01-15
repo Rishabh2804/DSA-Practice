98a444064a69aa48c76fe887f2e56e89d6cf5b2e
Rishabh Gupta <rishabhgupta2804@gmail.com>
2024-01-15 14:50:57 +0530

    Time: 79 ms (68.63%), Space: 90.6 MB (71.30%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
@@ -1,32 +1,46 @@
 class Solution {
+    
+    static final int OUT = 0;
+    static final int WIN_1 = 1;
+    static final int WIN_ALL = 2;
+    
+    // state change for every: 
+    // - win :   s -> s * 1
+    // - loss:   s -> s / 2
+    
     public List<List<Integer>> findWinners(int[][] matches) {
-        HashMap<Integer, Integer> players = new HashMap<>();
-        
+        HashMap<Integer, Integer> playerRecords = new HashMap<>();
         for(int[] match : matches){
-            int winner = match[0];
-            int loser = match[1];
             
-            if(!players.containsKey(winner))
-                players.put(winner, 0);
+            int w = match[0];
+            int l = match[1];
             
-            players.put(loser, players.getOrDefault(loser, 0) + 1);
+            playerRecords.putIfAbsent(w, WIN_ALL);
+            playerRecords.putIfAbsent(l, WIN_ALL);
+            
+            // no change
+            // playerRecords.put(w, playerRecords.get(w)); 
+            
+            playerRecords.put(l, playerRecords.get(l) / 2); // lose one {2 -> 1 -> 0}
         }
         
         List<List<Integer>> winners = new ArrayList<>();
-        winners.add(new ArrayList<>());
-        winners.add(new ArrayList<>());
         
-        for(Integer player : players.keySet()){
-            int losses = players.get(player);
-            if(losses == 0)
-                winners.get(0).add(player);
-            else if(losses == 1)
-                winners.get(1).add(player);
+        List<Integer> win_all = new ArrayList<>();
+        List<Integer> win_1 = new ArrayList<>();
+        
+        for(Integer player : playerRecords.keySet()){
+            Integer playerRecord = playerRecords.get(player);
+            if(playerRecord == WIN_ALL) win_all.add(player);
+            else if(playerRecord == WIN_1) win_1.add(player);
         }
         
-        Collections.sort(winners.get(0));
-        Collections.sort(winners.get(1));
-
+        Collections.sort(win_all);
+        Collections.sort(win_1);
+        
+        winners.add(win_all);
+        winners.add(win_1);
+        
         return winners;
     }
 }
