88c02b3906205b99437de7e6c04eb245647aba76
Rishabh Gupta <rishabhgupta2804@gmail.com>
2024-01-19 20:42:09 +0530

    Time: 8 ms (34.47%), Space: 45 MB (47.88%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,32 +1,27 @@
 class Solution {
-    public int minFallingPathSum(int[][] matrix) {    
+    public int minFallingPathSum(int[][] matrix) {
+        
         int n = matrix.length;
+        int m = matrix[0].length;
         
-        if(n == 1)
-            return matrix[0][0];
-        
-        int[][] pathSum = new int[n][n];
-        for(int i = 0; i < n; ++i)
-            pathSum[0][i] = matrix[0][i];
-        
-        for(int i = 1; i < n; ++i){
-            
-            pathSum[i][0] = matrix[i][0] + 
-                            Math.min(pathSum[i - 1][0], pathSum[i - 1][1]);
-            
-            for(int j = 1; j < n - 1; ++j)
-                pathSum[i][j] = matrix[i][j] + 
-                                Math.min(pathSum[i - 1][j],                                                         Math.min(pathSum[i - 1][j - 1], pathSum[i - 1][j + 1]));
-            
-            pathSum[i][n - 1] = matrix[i][n - 1] + 
-                                Math.min(pathSum[i - 1][n - 2], pathSum[i - 1][n - 1]);
-            
+        for(int i = n - 2; i >= 0; --i){
+            for(int j = 0; j < m; ++j){
+                
+                int minSum = Integer.MAX_VALUE;
+                if(j > 0) minSum = Math.min(minSum, matrix[i + 1][j - 1]);
+                
+                minSum = Math.min(minSum, matrix[i + 1][j]);
+                
+                if(j < m - 1) minSum = Math.min(minSum, matrix[i + 1][j + 1]);
+                
+                matrix[i][j] += minSum;
+            }
         }
         
-        int minPathSum = Integer.MAX_VALUE;
-        for(int i = 0; i < n; ++i)
-            minPathSum = Math.min(minPathSum, pathSum[n - 1][i]);
+        int minFallSum = Integer.MAX_VALUE;
+        for(int i : matrix[0])
+            minFallSum = Math.min(minFallSum, i);
         
-        return minPathSum;
+        return minFallSum;
     }
 }
