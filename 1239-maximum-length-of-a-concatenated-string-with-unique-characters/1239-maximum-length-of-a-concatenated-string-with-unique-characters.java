d7b02e970d9d28e0164fa79fae11a9fe33e6e191
Rishabh Gupta <rishabhgupta2804@gmail.com>
2024-01-23 14:54:54 +0530

    Time: 3 ms (95.97%), Space: 41 MB (94.56%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -1,61 +1,38 @@
 class Solution {
-    public int maxLength(List<String> arr) {
-        List<Integer> masks = new ArrayList<>();
-        for(String str : arr)
-            masks.add(stringToMask(str));
+    
+    private int solve(int i, List<Integer> masks, int currMask){
+        if(i >= masks.size()) return Integer.bitCount(currMask);
         
-        return solve(0, 0, masks);
+        int leave = solve(i + 1, masks, currMask);
+        
+        int take = 0;        
+        if((masks.get(i) & currMask) == 0){
+            currMask |= masks.get(i);
+            
+            take = solve(i + 1, masks, currMask);
+        } 
+        
+        return Math.max(leave, take);
     }
     
-    public int stringToMask(String str){
+    private int getMask(String str){
         
         int mask = 0;
         for(int i = 0; i < str.length(); ++i){
             int currBit = 1 << (str.charAt(i) - 'a');
-            if((currBit & mask) == 0)
-                mask |= currBit;
-            else
-                return 0;
             
+            if((mask & currBit) == 0) mask |= currBit;
+            else return 0;            
         }
-                
+        
         return mask;
     }
     
-    public int solve(int i, int currMask, List<Integer> masks){
-        if(i >= masks.size())
-            return length(currMask);
-                
-        int ans1 = solve(i + 1, currMask, masks);
-        int ans2 = 0;
-        if((currMask & masks.get(i)) == 0)
-            ans2 = solve(i + 1, (currMask | masks.get(i)), masks);
+    public int maxLength(List<String> arr) {
+        List<Integer> masks = new ArrayList<>();
+        for(String str : arr)
+            masks.add(getMask(str));
         
-        // System.out.println(masks.get(i) + " " + bits(currMask));
-        // System.out.println(masks.get(i) + " " + bits(currMask | masks.get(i)));
-        // System.out.println();
-                
-        return Math.max(ans1, ans2);
-    }
-    
-    public int length(int mask){
-        int count = 0;
-        while(mask != 0){
-            count += (mask & 1);
-            mask >>= 1;
-        }
-        
-        return count;
-    }
-    
-    public List<Integer> bits(int n){
-        List<Integer> bit = new ArrayList<>();
-        while(bit.size() < 26){
-            bit.add(n & 1);
-            n >>= 1;
-        }
-        
-        Collections.reverse(bit);
-        return bit;
+        return solve(0, masks, 0);
     }
 }
