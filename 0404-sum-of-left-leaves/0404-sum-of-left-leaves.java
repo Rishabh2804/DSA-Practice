6d757a33dc1d79e9a3b7c134816bf79333144b13
Rishabh Gupta <rishabhgupta2804@gmail.com>
2024-04-14 13:21:40 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (32.86%) - LeetHub

---
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    private boolean isLeaf(TreeNode root){
+        if(root == null) return false;
+        return root.left == null && root.right == null;
+    }
+    
+    public int sumOfLeftLeaves(TreeNode root) {
+        if(root == null) return 0;
+        
+        int sum = sumOfLeftLeaves(root.left);
+        if(isLeaf(root.left)) sum += root.left.val;
+        if(!isLeaf(root.right)) sum += sumOfLeftLeaves(root.right);
+        
+        return sum;
+    }
+}
