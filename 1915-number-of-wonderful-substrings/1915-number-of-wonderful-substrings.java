0027f6a1b2e8ca610b6a958dc0422f2dc3f8f921
Rishabh Gupta <rishabhgupta2804@gmail.com>
2024-05-01 11:53:21 +0530

    Time: 961 ms (5.71%), Space: 48.5 MB (7.62%) - LeetHub

--- 1915-number-of-wonderful-substrings/1915-number-of-wonderful-substrings.java
+++ 1915-number-of-wonderful-substrings/1915-number-of-wonderful-substrings.java
@@ -1,34 +1,41 @@
 class Solution {
-    public long wonderfulSubstrings(String word) {
-        int N = word.length();
-
-        // Create the frequency map
-        // Key = bitmask, Value = frequency of bitmask key
-        Map<Integer, Integer> freq = new HashMap<>();
-
-        // The empty prefix can be the smaller prefix, which is handled like this
-        freq.put(0, 1);
-
-        int mask = 0;
-        long res = 0L;
-        for (int i = 0; i < N; i++) {
-            char c = word.charAt(i);
-            int bit = c - 'a';
-
-            // Flip the parity of the c-th bit in the running prefix mask
-            mask ^= (1 << bit);
-            
-            // Count smaller prefixes that create substrings with no odd occurring letters
-            res += freq.getOrDefault(mask, 0);
-
-            // Increment value associated with mask by 1
-            freq.put(mask, freq.getOrDefault(mask, 0) + 1);
-
-            // Loop through every possible letter that can appear an odd number of times in a substring
-            for (int odd_c=0; odd_c < 10; odd_c++) {
-                res += freq.getOrDefault(mask ^ (1 << odd_c),0);
-            }
+    
+    private List<Integer> getDiffMasks(int currMask) {
+        
+        List<Integer> masks = new ArrayList<>();
+        for(int i = 0; i < 26; ++i){
+            int bit = 1 << i;
+            int mask = currMask ^ bit;
+            masks.add(mask);
         }
-        return res;
+        
+        return masks;
+    }
+    
+    public long wonderfulSubstrings(String word) {
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        
+        long count = 0;
+        
+        int currMask = 0;
+        for(int i = 0; i < word.length(); ++i){
+            int curr = word.charAt(i) - 'a';
+            currMask ^= 1 << curr;
+            
+            List<Integer> masks = getDiffMasks(currMask);
+            for(int mask : masks){
+                int currCount = map.getOrDefault(mask, 0);
+                count += currCount;
+            }
+            
+            int currCount = map.getOrDefault(currMask, 0);
+            count += currCount;
+            
+            map.put(currMask, currCount + 1);
+        }
+        
+        return count;
     }
 }
