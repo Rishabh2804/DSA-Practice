56df84f385fa572de3df8db261360aee4c5e6ffa
Rishabh Gupta <rishabhgupta2804@gmail.com>
2024-05-04 12:08:30 +0530

    Time: 16 ms (97.17%), Space: 54.2 MB (55.93%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -13,7 +13,7 @@ class Solution {
                 si ++;
                 ei --;
             } else
-                ei--;            
+                ei--;
         }
        
         return boats;
