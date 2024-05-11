94c56af54870a44f460ed2d2535457f55e363751
Rishabh Gupta <rishabhgupta2804@gmail.com>
2024-05-12 00:15:49 +0530

    Time: 456 ms (45.41%), Space: 68.6 MB (54.15%) - LeetHub

---
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] kthSmallestPrimeFraction(int[] arr, int k) {        
+        int n = arr.length;
+        
+        PriorityQueue<int[]> fracs = new PriorityQueue<>((a, b) -> a[0] * b[1] - b[0] * a[1]);
+        
+        for(int i = 0; i < n; ++i){
+            for(int j = i + 1; j < n; ++j) fracs.add(new int[] {arr[i], arr[j]});
+        }
+        
+        while(k --> 1) fracs.poll();
+        
+        return fracs.poll();
+    }
+}
