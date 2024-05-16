7e9bb18ecec86693fac409dc3bbfcef3f5b5df50
Rishabh Gupta <rishabhgupta2804@gmail.com>
2024-05-17 00:38:51 +0530

    Time: 0 ms (100.00%), Space: 44 MB (80.35%) - LeetHub

--- 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.java
+++ 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.java
@@ -14,14 +14,12 @@
  * }
  */
 class Solution {
-    public boolean evaluateTree(TreeNode root) {
-        
+    public boolean evaluateTree(TreeNode root) {        
         if(root.left == null && root.right == null) return root.val == 1;
         
-        boolean left = evaluateTree(root.left);
-        boolean right = evaluateTree(root.right);
-        
-        if(root.val == 2) return left || right;
-        else return left && right;
+        if(root.val == 2)
+            return evaluateTree(root.left) || evaluateTree(root.right);
+        else // if(root.val == 3) 
+            return evaluateTree(root.left) && evaluateTree(root.right);
     }
 }
