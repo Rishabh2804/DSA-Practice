aab9e1ae17ff48d854994524489630e5c938fbf6
Rishabh Gupta <rishabhgupta2804@gmail.com>
2024-05-18 12:47:08 +0530

    Time: 57 ms (80.06%), Space: 54.9 MB (80.70%) - LeetHub

---
+++ 2875-minimum-size-subarray-in-infinite-array/2875-minimum-size-subarray-in-infinite-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minSizeSubarray(vector<int>& n, long long t)
+{
+	long long s = reduce(begin(n), end(n), 0ll);
+	int quot(t/s), sz(size(n));
+	t = t%s;
+	int out{INT_MAX};
+	for(int i{}, j{}; i<size(n); )
+		if(t>0)
+		{
+			t -= n[j%sz];
+			++j;
+		 }
+		else
+		{
+		    if(!t) out=min(out,(j-i+sz)%sz);
+		    t += n[i++];                
+		}
+	return out==INT_MAX ? -1 : quot*sz+out;
+}
+};
