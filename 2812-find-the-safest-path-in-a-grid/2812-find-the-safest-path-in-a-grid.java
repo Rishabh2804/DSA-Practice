61a48c2f66ec9774edd68c01e5dbd4e19ebaeae8
Rishabh Gupta <rishabhgupta2804@gmail.com>
2024-05-20 00:38:06 +0530

    Time: 2678 ms (5.02%), Space: 72.3 MB (17.23%) - LeetHub

---
+++ 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.java
@@ -0,0 +1,136 @@
+class Solution {
+    
+    private static final int INF = 10000;
+    private static final int[][] dirs = {
+        {-1, 0}, {0, 1}, {0, -1}, {1, 0}
+    };
+    
+    private void print(int[][] arr){
+        for(int[] row : arr){
+            for(int c : row) System.out.print(c + " ");
+            
+            System.out.println();
+        }
+        System.out.println();
+    }
+    
+    private boolean xor(boolean a, boolean b){
+        return (a || b) && !(a && b);
+    }    
+    
+    private int[][] getSafetyMatrix(List<List<Integer>> grid){
+        
+        int n = grid.size();
+        int[][] safety = new int[n][n];
+        for(int[] row : safety) Arrays.fill(row, INF);
+        
+        Queue<int[]> cells = new LinkedList<>();
+        for(int i = 0; i < n; ++i){
+            for(int j = 0; j < n; ++j){
+                if(grid.get(i).get(j) == 1) {
+                    cells.add(new int[]{i, j});
+                    safety[i][j] = 0;
+                }
+            }
+        }
+        
+        while(!cells.isEmpty()){
+            int size = cells.size();
+            while(size --> 0){
+                int[] curr = cells.poll();
+                
+                int i = curr[0];
+                int j = curr[1];
+                
+                for(int[] dir : dirs){
+                    int x = i + dir[0];
+                    int y = j + dir[1];
+                    
+                    if(0 <= x && x < n && 0 <= y && y < n){
+                        if(safety[x][y] != INF) continue;
+
+                        safety[x][y] = 1 + safety[i][j];
+                        cells.add(new int[]{x, y});
+                    }
+                }                                
+            }
+        }
+        
+        print(safety);
+        return safety;
+    }
+    
+    private boolean hasPath(int target, int[][] safety){
+        
+        int n = safety.length;
+        
+        if(safety[0][0] < target || safety[n - 1][n - 1] < target) 
+            return false;
+        
+        Queue<int[]> nodes = new LinkedList<>();
+        nodes.add(new int[]{0, 0});
+        
+        boolean[][] vis = new boolean[n][n];
+        vis[0][0] = true;
+        while(!nodes.isEmpty()){
+            
+            int size = nodes.size();
+            while(size --> 0){
+                
+                int[] curr = nodes.poll();
+                
+                if(curr[0] == n - 1 && curr[1] == n - 1) return true;
+                
+                for(int[] dir : dirs){
+                    int x = curr[0] + dir[0];
+                    int y = curr[1] + dir[1];
+                    
+                    if(x < 0 || x >= n || y < 0 || y >= n) continue;                    
+                    if(vis[x][y]) continue;
+                    if(safety[x][y] < target) continue;
+                    
+                    vis[x][y] = true;
+                    nodes.add(new int[]{x, y});
+                }
+            }
+        }
+        
+        return false;
+    }
+    
+    private int solve(int i, int j, int[][] safety, boolean[][] visited){
+        if(i < 0 || j < 0 || i >= safety.length || j >= safety.length) return -1;
+        
+        if(i == safety.length - 1 && j == safety.length - 1) return safety[i][j];        
+        if(visited[i][j]) return -1;
+        
+        visited[i][j] = true;
+        
+        int currSafety = 0;
+        for(int[] dir : dirs){
+            int x = i + dir[0];
+            int y = j + dir[1];
+            
+            int rest = solve(x, y, safety, visited);
+            currSafety = Math.max(currSafety, rest);
+        }
+        
+        visited[i][j] = false;
+        return Math.min(currSafety, safety[i][j]);
+    }
+    
+    public int maximumSafenessFactor(List<List<Integer>> grid) {
+        
+        int n = grid.size();
+        int[][] safety = getSafetyMatrix(grid);
+        
+        int l = 0, r = 2 * (n - 1);
+        while(l < r){
+            int mid = (l + r + 1) / 2;
+            if(hasPath(mid, safety)) l = mid;
+            else r = mid - 1;
+        }
+        
+        return r;
+    }
+}
