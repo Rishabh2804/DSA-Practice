05529398a19f3130044f4a7729b2ee77a5c280d1
Rishabh Gupta <rishabhgupta2804@gmail.com>
2024-06-29 00:22:36 +0530

    Time: 51 ms (22.16%), Space: 72.9 MB (10.27%) - LeetHub

---
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.java
@@ -0,0 +1,35 @@
+class Solution {
+    public long maximumImportance(int n, int[][] roads) {
+        HashMap<Integer, Integer> values = new HashMap<>();
+        
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i < n; ++i) adj.add(new ArrayList<>());
+        
+        int[] degree = new int[n];
+        for(int[] road : roads){
+            int u = road[0];
+            int v = road[1];
+            
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> adj.get(b).size() - adj.get(a).size());
+        for(int i = 0; i < n; ++i) pq.add(i);
+        
+        long total = 0;
+        int val = n;
+        while(!pq.isEmpty()){
+            int curr = pq.poll();
+            
+            
+            int rem = adj.get(curr).size();
+            // System.out.println(curr + " " + rem);
+            
+            total += 1l * val * rem;
+            val--;
+        }
+        
+        return total;
+    }
+}
