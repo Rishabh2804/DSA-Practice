9533525409e652a70331a11d78fcd4ffa6f56938
Rishabh Gupta <rishabhgupta2804@gmail.com>
2024-06-29 00:23:10 +0530

    Time: 47 ms (24.32%), Space: 73.3 MB (9.73%) - LeetHub

--- 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.java
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.java
@@ -24,8 +24,6 @@ class Solution {
             
             
             int rem = adj.get(curr).size();
-            // System.out.println(curr + " " + rem);
-            
             total += 1l * val * rem;
             val--;
         }
