b6b36a25e80d9e40b833b88cfeb2409d4a87176d
Rishabh Gupta <rishabhgupta2804@gmail.com>
2024-07-17 23:50:25 +0530

    Time: 0 ms (100.00%), Space: 44.4 MB (93.36%) - LeetHub

--- 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.java
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.java
@@ -14,54 +14,32 @@
  * }
  */
 class Solution {
-    static final int DEL_LENGTH = 1001;
-    static List<TreeNode> forest;
     
-    public void solve(TreeNode root, boolean[] del){
+    private List<TreeNode> list;
+    
+    private void solve(TreeNode root, boolean[] dlt){
         if(root == null) return;
         
-        solve(root.left, del);
-        solve(root.right, del);
-        
-        if(root.left != null && del[root.left.val]){
-            if(root.left.left != null)
-                forest.add(root.left.left);
-            
-            if(root.left.right != null)
-                forest.add(root.left.right);
-            
-            root.left = null;
+        if(dlt[root.val]){
+            if(root.left != null && !dlt[root.left.val]) list.add(root.left);
+            if(root.right != null && !dlt[root.right.val]) list.add(root.right);            
         }
         
-        if(root.right != null && del[root.right.val]){
-            if(root.right.left != null)
-                forest.add(root.right.left);
-            
-            if(root.right.right != null)
-                forest.add(root.right.right); 
-            
-            root.right = null;
-        }                
+        solve(root.left, dlt);
+        solve(root.right, dlt);
+        
+        if(root.left != null && dlt[root.left.val]) root.left = null;
+        if(root.right != null && dlt[root.right.val]) root.right = null;
     }
     
     public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
-        forest = new LinkedList<>();
-        if(root == null) return forest;
+        list = new ArrayList<>();
+        boolean[] dlt = new boolean[1001];
+        for(int i : to_delete) dlt[i] = true;
         
-        boolean[] del = new boolean[DEL_LENGTH];
-        for(int tree = 0; tree < to_delete.length; ++tree)
-            del[to_delete[tree]] = true; 
+        solve(root, dlt);
+        if(root != null && !dlt[root.val]) list.add(root);
         
-        solve(root, del);
-        if(del[root.val]){
-            if(root.left != null)
-                forest.add(root.left);
-            
-            if(root.right != null)
-                forest.add(root.right);   
-        } else
-            forest.add(root);
-        
-        return forest;
+        return list;
     }
 }
