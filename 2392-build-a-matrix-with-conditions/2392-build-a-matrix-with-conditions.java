4996386fd59d4bf2b8717f497f448cd01295bb95
Rishabh Gupta <rishabhgupta2804@gmail.com>
2024-07-21 11:09:31 +0530

    Time: 9 ms (93.70%), Space: 55.8 MB (6.30%) - LeetHub

---
+++ 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.java
@@ -0,0 +1,69 @@
+class Solution {
+    
+    private static final int[][] NONE = new int[0][0];
+    
+    private int[] getMap(int[] vals){
+        if(vals == null) return null;
+        int[] map = new int[vals.length];
+        for(int i = 0; i < vals.length; ++i){
+            map[vals[i]] = i;
+        }
+        
+        return map;
+    }
+    
+    private int[] topoSort(int k, int[][] edges){
+        int[] vals = new int[k];
+        
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i < k; ++i) adj.add(new ArrayList<>());
+        
+        int[] in = new int[k];
+        for(int[] edge : edges){
+            int u = edge[0] - 1;
+            int v = edge[1] - 1;
+            
+            in[v]++;
+            
+            adj.get(u).add(v);
+        }
+        
+        Queue<Integer> nodes = new LinkedList<>();
+        for(int i = 0; i < k; ++i){
+            if(in[i] == 0) nodes.add(i);        
+        }
+        
+        if(nodes.isEmpty()) return null;
+        
+        int j = 0;
+        while(!nodes.isEmpty()){
+            int curr = nodes.poll();
+            vals[j++] = curr;
+            
+            for(int nbr : adj.get(curr)){                
+                in[nbr]--;
+                if(in[nbr] == 0) nodes.add(nbr);                
+            }
+        }
+        
+        if(j < k) return null;
+        return getMap(vals);
+    }
+    
+    public int[][] buildMatrix(int k, int[][] rowConditions, int[][] colConditions) {
+        
+        int[] row = topoSort(k, rowConditions);
+        int[] col = topoSort(k, colConditions);
+        
+        if(row == null || col == null) return NONE;
+        int[][] mat = new int[k][k];
+        for(int i = 0; i < k; ++i){
+            int r = row[i];
+            int c = col[i];
+            
+            mat[r][c] = i + 1;
+        }
+        
+        return mat;
+    }
+}
