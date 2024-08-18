6d463c1bae49ece75e93ad76dc30adcc777f0091
Rishabh Gupta <rishabhgupta2804@gmail.com>
2024-08-18 13:55:39 +0530

    Time: 4 ms (36.10%), Space: 41.2 MB (88.93%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.java
+++ 0264-ugly-number-ii/0264-ugly-number-ii.java
@@ -2,16 +2,20 @@ class Solution {
     
     private static final int[] factors = {2, 3, 5};
     
-    public int nthUglyNumber(int n) {
+    private static final List<Integer> res;
+    
+    static {
+        res = new ArrayList<>();
         
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        pq.add(1);
-        
         HashSet<Integer> vis = new HashSet<>();
+        pq.add(1);        
         vis.add(1);        
         
+        int n = 1691;
         while(n > 1){
             int curr = pq.poll();            
+            res.add(curr);
             
             for(int fact : factors){
                 long temp = 1L * curr * fact;
@@ -28,6 +32,38 @@ class Solution {
             n--;
         }
         
-        return pq.poll();
+        Collections.sort(res);        
+    }
+    
+    public int nthUglyNumber(int n) {
+        
+//         PriorityQueue<Integer> pq = new PriorityQueue<>();
+//         pq.add(1);
+        
+//         HashSet<Integer> vis = new HashSet<>();
+//         vis.add(1);        
+        
+//         while(n > 1){
+//             int curr = pq.poll();            
+            
+//             for(int fact : factors){
+//                 long temp = 1L * curr * fact;
+                
+//                 if(temp >= Integer.MAX_VALUE) break;
+                
+//                 int curr1 = (int) temp;
+//                 if(!vis.contains(curr1)) {
+//                     pq.add(curr1);
+//                     vis.add(curr1);
+//                 }
+//             }                        
+            
+//             n--;
+//         }
+        
+        
+        // return pq.poll();
+        
+        return res.get(n - 1);
     }
 }
