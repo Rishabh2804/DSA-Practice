e936b1a70ae79e5882773773384e0bd6c141ec89
Rishabh Gupta <rishabhgupta2804@gmail.com>
2024-08-27 01:13:39 +0530

    Time: 0 ms (100.00%), Space: 44.8 MB (21.45%) - LeetHub

---
+++ 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.java
@@ -0,0 +1,41 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+    
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+    
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    
+    private void solve(Node root, List<Integer> list){
+        if(root == null) return;
+        
+        if(root.children == null) {
+            list.add(root.val);
+            return;
+        }
+        
+        for(Node child : root.children)
+            solve(child, list);
+        
+        list.add(root.val);        
+    }
+    
+    public List<Integer> postorder(Node root) {
+        List<Integer> list = new ArrayList();
+        solve(root, list);        
+        return list;
+    }
+}
