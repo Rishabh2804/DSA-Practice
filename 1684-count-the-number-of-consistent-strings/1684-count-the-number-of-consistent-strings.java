c3912ad8dab5995fdf741c36b1b1a828001934e3
Rishabh Gupta <rishabhgupta2804@gmail.com>
2024-09-13 00:06:30 +0530

    Time: 5 ms (96.90%), Space: 45.3 MB (62.99%) - LeetHub

--- 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.java
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.java
@@ -1,17 +1,27 @@
 class Solution {
     
+    private static final int SIZE = 26;
+    private static final int[] MASKS;
+    static {
+        
+        MASKS = new int[SIZE];
+        for(int i = 0; i < SIZE; ++i)
+            MASKS[i] = 1 << i;
+                
+    }
+    
     private int generateMask(String allowed){
         int mask = 0;
         
         for(int i = 0; i < allowed.length(); ++i)
-            mask |= 1 << (allowed.charAt(i) - 'a');
+            mask |= MASKS[allowed.charAt(i) - 'a'];
         
         return mask;
     }
     
     private boolean isConsistent(String word, int mask){
         for(int i = 0; i < word.length(); ++i){
-            int curr = 1 << (word.charAt(i) - 'a');
+            int curr = MASKS[word.charAt(i) - 'a'];
             if((curr & mask) == 0) return false;
         }
         
