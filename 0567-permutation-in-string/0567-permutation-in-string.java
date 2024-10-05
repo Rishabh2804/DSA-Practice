6f602d45c57b5db4be17b8d184fb621b3a90b868
Rishabh Gupta <rishabhgupta2804@gmail.com>
2024-10-05 23:20:57 +0530

    Time: 6 ms (86.36%), Space: 42.4 MB (88.85%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,33 +1,41 @@
 class Solution {
-    
-    private boolean match(int[] arr1, int[] arr2){
-        for(int i = 0; i < arr1.length; ++i)
-            if(arr1[i] != arr2[i]) return false;
         
-        return true;
-    }
-    
-    public boolean checkInclusion(String s1, String s2) {
-        if(s2.length() < s1.length()) return false;
-        
+    public boolean checkInclusion(String s1, String s2) {        
+        if(s1.length() > s2.length())
+            return false;
+         
         int[] freq1 = new int[26];
         int[] freq2 = new int[26];
         
-        for(int i = 0; i < s1.length(); ++i){
+        for(int i = 0; i < s1.length(); ++i)
             freq1[s1.charAt(i) - 'a']++;
-            freq2[s2.charAt(i) - 'a']++;
-        }
         
-        if(match(freq1, freq2)) return true;
+        for(int i = 0; i < s1.length(); ++i)
+            freq2[s2.charAt(i) - 'a']++;
+        
+        if(isIdentical(freq1, freq2)) return true;
         
         for(int i = s1.length(); i < s2.length(); ++i){
+            int prevIdx = s2.charAt(i - s1.length()) - 'a';
+            int currIdx = s2.charAt(i) - 'a';
             
-            freq2[s2.charAt(i - s1.length()) - 'a']--;
-            freq2[s2.charAt(i) - 'a']++;
+            freq2[prevIdx]--;
+            freq2[currIdx]++;
             
-            if(match(freq1, freq2)) return true;
+            if(isIdentical(freq1, freq2))
+                return true;
+        }
+            
+        return false;
+    }
+    
+    private boolean isIdentical(int[] arr1, int[] arr2){
+        
+        for(int i = 0; i < arr1.length; ++i){
+            if(arr1[i] != arr2[i])
+                return false;                        
         }
         
-        return false;
+        return true;
     }
 }
