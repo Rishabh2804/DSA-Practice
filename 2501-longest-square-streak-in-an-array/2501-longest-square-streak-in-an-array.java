6f0804bcda0276c09523e35a36f4863b2925e829
Rishabh Gupta <rishabhgupta2804@gmail.com>
2024-10-28 16:53:59 +0530

    Time: 47 ms (56.25%), Space: 60.4 MB (55.56%) - LeetHub

--- 2501-longest-square-streak-in-an-array/2501-longest-square-streak-in-an-array.java
+++ 2501-longest-square-streak-in-an-array/2501-longest-square-streak-in-an-array.java
@@ -5,30 +5,53 @@ class Solution {
     private static final int MIN_LEN = 2;
     private static final int MAX = 46341; // sqrt of Integer.MAX_VALUE
     
-    private int solve(int curr, HashMap<Integer, Integer> dp){
-        if(!dp.containsKey(curr)) return 0;
+    private void print(int[] arr){
+        for(int i : arr)
+            System.out.print(i + " ");
         
-        if(dp.get(curr) > DEF_LEN) return dp.get(curr);
+        System.out.println();
+    }
+    
+    private int lowerBound(int x, int si, int[] arr){
         
-        int len = 1 + solve(curr * curr, dp);
-        dp.put(curr, len);
+        int ei = arr.length - 1;
+        while(si < ei){
+            
+            int mid = (si + ei) / 2;
+            if(arr[mid] >= x) ei = mid;
+            else si = mid + 1;
+        }
         
-        return len;
+        if(si >= arr.length || arr[si] != x) return NONE;
+        return si;
+    }
+    
+    private int solve(int i, int[] nums, int[] dp){
+        if(i >= nums.length) return DEF_LEN;
+        
+        if(dp[i] != DEF_LEN) return dp[i];
+        
+        int curr = nums[i];
+        if(curr > MAX) return DEF_LEN;
+        
+        int next = lowerBound(curr * curr, i, nums);
+        if(next == NONE) return DEF_LEN;
+        
+        return dp[i] = 1 + solve(next, nums, dp);
     }
     
     public int longestSquareStreak(int[] nums) {
-        HashMap<Integer, Integer> dp = new HashMap<>();
-        for(int i : nums) dp.put(i, DEF_LEN);
+        Arrays.sort(nums);
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, DEF_LEN);
         
-        int maxLen = DEF_LEN;
-        for(int num : dp.keySet()){
-            if(num > MAX) continue;
-            
-            int len = solve(num, dp);
-            maxLen = Math.max(maxLen, len);
+        int max = DEF_LEN;
+        for(int i = 0; i < nums.length; ++i){
+            int currLen = solve(i, nums, dp);
+            max = Math.max(max, currLen);
         }
         
-        if(maxLen < MIN_LEN) return NONE;
-        return maxLen;
+        if(max < MIN_LEN) return NONE;
+        return max;
     }
 }
